ca222b827d7fccd5ebef56077b104afa538268da
JessicaRanto <121685144+JessicaRanto@users.noreply.github.com>
2023-04-06 22:47:49 +0200

    Update RewardValue.java

--- src/main/RewardValue.java
+++ src/main/RewardValue.java
@@ -1,4 +1,30 @@
 public class RewardValue {
+   void cash() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    void miles() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+
+    void convert_from_cash_to_miles() {
+        assert false;
+    }
+
+
+    void convert_from_miles_to_cash() {
+        assert false;
+    }
+}
+
+
+
+
    
 
-}
+
