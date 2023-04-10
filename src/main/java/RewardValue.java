4795344a88032eb5eacda278c558222af3d54b87
Christopher Terrano <mterrano1@yahoo.com>
2023-04-10 14:21:14 -0500

    create RewardValue constructors

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,4 +2,14 @@ public class RewardValue {
     private final double cashValue;
     // Conversion rate from miles to cash (1 mile = 0.0035 cash)
     private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    // Accepts a cash value and initializes the cashValue instance variable
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    // Accepts a value in miles, converts it to cash value, and initializes the cashValue instance variable
+    public RewardValue(int milesValue) {
+        this.cashValue = milesToCash(milesValue);
+    }
 }
