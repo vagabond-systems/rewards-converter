d14e6fa650cd89fb5011f64e05035c24932adc87
Christopher Terrano <mterrano1@yahoo.com>
2023-04-10 14:13:57 -0500

    create cashValue instance variable and conversion rate

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,3 +1,5 @@
 public class RewardValue {
-    
+    private final double cashValue;
+    // Conversion rate from miles to cash (1 mile = 0.0035 cash)
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 }
