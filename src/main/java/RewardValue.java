6471eb4d5d27fd751ad969dd82efab7af9cd8edc
Anthony Bertani <anthonybertani1@gmail.com>
2023-04-11 12:22:58 -0400

    RewardsValue class tests created

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,37 @@
+
 public class RewardValue {
+    private double cash;
+    private int miles;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cash) {
+        if(cash < 0) {
+            throw new IllegalArgumentException("Cash value must be positive");
+        }
+        this.cash = cash;
+    }
+
+    public RewardValue(int miles) {
+        if(miles < 0) {
+            throw new IllegalArgumentException("Miles value must be positive");
+        }
+        this.miles = miles;
+    }
+
+    public double getCashValue() {
+        return miles * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public int getMilesValue() {
+        return (int) (cash / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    public double getCash() {
+        return cash;
+    }
+
+    public int getMiles() {
+        return miles;
+    }
+
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RewardValueTests {
 
@@ -8,23 +9,56 @@ public class RewardValueTests {
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCash());
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        assertEquals(milesValue, rewardValue.getMiles());
+    }
+
+    @Test
+    void create_with_negative_miles_value_throws_IllegalArgumentException() {
+        int milesValue = -10000;
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                                var rewardValue = new RewardValue(milesValue);
+                              });
+
+        String expectedMessage = "Miles value must be positive";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void create_with_negative_cash_value_throws_IllegalArgumentException() {
+        double cashValue = -100;
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            var rewardValue = new RewardValue(cashValue);
+        });
+
+        String expectedMessage = "Cash value must be positive";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMiles = 28571;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double expectedCash = 35.00;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCash, rewardValue.getCashValue());
     }
+
 }
