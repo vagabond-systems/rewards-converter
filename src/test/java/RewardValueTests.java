f3a5c71bea97d9d4ba5c858a9473aca69b0644d8
wilfredo-flores <willflores1221@gmail.com>
2023-04-11 06:59:56 -0400

    Add: miles/cash conversion tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100.0;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(28571, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 28571;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(100.0, rewardValue.getCashValue(), 0.1);
     }
 }
