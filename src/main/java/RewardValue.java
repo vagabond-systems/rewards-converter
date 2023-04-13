dd61797bb73f8a2559f5af8a8c595d7b053dc476
donald-langston <donald_langston@yahoo.com>
2023-04-13 16:28:52 -0400

    Added unit tests for conversion rate

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,6 +2,8 @@ public class RewardValue {
 
     private double cashValue;
     private int milesValue;
+
+
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
@@ -10,11 +12,19 @@ public class RewardValue {
         this.milesValue = milesValue;
     }
 
+    public double getCashValue() {
+        return cashValue;
+    }
+
     public int getMilesValue() {
+        return milesValue;
+    }
+
+    public int convert_from_cash_to_miles() {
         return (int) (cashValue / 0.0035);
     }
 
-    public double getCashValue() {
+    public double convert_from_miles_to_cash() {
         return milesValue * 0.0035;
     }
 }
