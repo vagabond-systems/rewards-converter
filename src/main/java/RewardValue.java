111c1ae78a27ecb7f6dfce9757764f9acabbb841
dennis-22-csc <dennisakpotaire@gmail.com>
2023-04-14 09:12:20 +0100

    Refactored the name given to the instance variables of the RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,56 +1,60 @@
 /** Represents a reward value.
  * @author Akpotaire Dennis
- * @version 1.0
+ * @version 1.3
  * @since 1.0
  */
 public class RewardValue {
-    private double cash;
-    private int mile;
+    private double cashValue;
+    private int milesValue;
     public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     /** Creates a reward value based on cash.
-     * @param cash The cash value to be converted to a reward value.
+     *
+     * @param cashValue The cash value to be converted to a reward value.
      */
-    public RewardValue(double cash){
-        this.cash = cash;
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
     }
 
     /** Creates a reward value based on miles.
-     * @param mile The mile value to be converted to cash.
+     *
+     * @param milesValue The mile value to be converted to cash.
      */
-    public RewardValue(int mile){
-        this.mile = mile;
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
     }
 
     /** Gets the cash value associated with a particular mile value.
+     *
      * @return A double value of the cash value associated with a particular mile or the original cash value.
      */
     public double getCashValue(){
-        return mile == 0 ? cash : convertToCash(mile);
+        return milesValue == 0 ? cashValue : convertToCash(milesValue);
     }
 
     /** Gets the mile value associated with a particular cash value.
+     *
      * @return An int value of the mile value associated with a particular cash value or the original mile value.
      */
     public int getMilesValue(){
-        return cash == 0 ? mile : convertToMiles(cash);
+        return cashValue == 0 ? milesValue : convertToMiles(cashValue);
     }
 
     /** Convert mile value to cash value
      *
-     * @param mile The mile value to be converted.
+     * @param milesValue The mile value to be converted.
      * @return The cash value after conversion of type double.
      */
-    private double convertToCash(int mile){
-        return (double) Math.round(mile * MILES_TO_CASH_CONVERSION_RATE * 100) / 100;
+    private double convertToCash(int milesValue){
+        return (double) Math.round(milesValue * MILES_TO_CASH_CONVERSION_RATE * 100) / 100;
     }
 
     /** Convert cash value to miles value
      *
-     * @param cash The cash value to be converted.
+     * @param cashValue The cash value to be converted.
      * @return The miles value after conversion of type int.
      */
-    private int convertToMiles(double cash){
-        return (int) Math.ceil((int) Math.round(cash / MILES_TO_CASH_CONVERSION_RATE));
+    private int convertToMiles(double cashValue){
+        return (int) Math.ceil((double) Math.round(cashValue / MILES_TO_CASH_CONVERSION_RATE));
     }
 }
