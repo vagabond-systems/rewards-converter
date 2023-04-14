2662cbccdc61034ed5b384334e8cdcdfeaf85cbf
dennis-22-csc <dennisakpotaire@gmail.com>
2023-04-14 08:54:31 +0100

    Replaced magic number 0.0035 with numeric constant

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -6,6 +6,7 @@
 public class RewardValue {
     private double cash;
     private int mile;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     /** Creates a reward value based on cash.
      * @param cash The cash value to be converted to a reward value.
@@ -25,14 +26,14 @@ public class RewardValue {
      * @return A double value of the cash value associated with a particular mile or the original cash value.
      */
     public double getCashValue(){
-        return mile == 0 ? cash : (double) Math.round(mile * 0.0035 * 100) / 100;
+        return mile == 0 ? cash : (double) Math.round(mile * MILES_TO_CASH_CONVERSION_RATE * 100) / 100;
     }
 
     /** Gets the mile value associated with a particular cash value.
      * @return An int value of the mile value associated with a particular cash value or the original mile value.
      */
     public int getMilesValue(){
-        return cash == 0 ? mile : (int) Math.ceil((int) Math.round(cash / 0.0035));
+        return cash == 0 ? mile : (int) Math.ceil((int) Math.round(cash / MILES_TO_CASH_CONVERSION_RATE));
     }
 
 }
