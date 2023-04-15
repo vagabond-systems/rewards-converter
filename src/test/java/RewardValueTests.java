f7c5d0666c5e2fcca35b58189a6965407be0d38b
Hectorrrra <hmanueljr027@hotmail.com>
2023-04-15 11:39:16 -0700

    Second task finished

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -21,11 +21,23 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / 0.0035);
+
+        var rewardValue = new RewardValue(cashValue);
+        int actualMilesValue = rewardValue.getMilesValue();
+
+        assertEquals(expectedMilesValue, actualMilesValue);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double expectedCashValue = milesValue * 0.0035;
+
+        var rewardValue = new RewardValue(milesValue);
+        double actualCashValue = rewardValue.getCashValue();
+
+        assertEquals(expectedCashValue, actualCashValue);
     }
 }
