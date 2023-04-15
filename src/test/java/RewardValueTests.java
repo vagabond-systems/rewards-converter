ea450732500fed357b2c2d3f10192bc15e29f83f
nihith1801 <122144472+nihith1801@users.noreply.github.com>
2023-04-16 00:16:57 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,64 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RewardValueTests {
+public class RewardValue {
+
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue * 0.01);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue / 0.01;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+
+    public int fromCashToMiles(double cashValue) {
+        return (int) (cashValue * 0.01);
+    }
+
+    public double fromMilesToCash(int milesValue) {
+        return milesValue / 0.01;
+    }
 
     @Test
-    void create_with_cash_value() {
+    void testCreateWithCashValue() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
+        RewardValue rewardValue = new RewardValue(cashValue);
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
-    void create_with_miles_value() {
+    void testCreateWithMilesValue() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        RewardValue rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    void testFromCashToMiles() {
+        double cashValue = 100;
+        int expectedMilesValue = 10000;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+    void testFromMilesToCash() {
+        int milesValue = 10000;
+        double expectedCashValue = 100;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
