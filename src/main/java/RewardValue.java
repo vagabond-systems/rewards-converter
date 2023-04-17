c03f30dcb951fe0b2644755ccac294b3f0105860
Alister <alisterdsouzaa@outlook.com>
2023-04-17 14:51:46 +0530

    Created a RewardValue class for RewardsConverter main(). Created Unit test to check the functionality of the program.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,31 @@
 public class RewardValue {
     /* Private Instance variables */
-    private double cashValue;
-    private float milesValue;
+    private final double cashValue;
+    public static final double CONVERSION_RATE = 0.0035;
 
-    /* Constructor for milesValue */
-    public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
-    }
     /* Constructor for cashValue */
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+    }
+    /* Constructor for milesValue */
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+    /* Conversion method for getMilesValue :By converting to cashValue at a rate of 0.0035 */
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * CONVERSION_RATE;
     }
 
     /* Get method for getCashValue */
     public double getCashValue() {
         return cashValue;
     }
-
-    /* Conversion method for getMilesValue :By converting to cashValue at a rate of 0.0035 */
-    public float getMilesValue() {
-        return (float) (cashValue * 0.0035);
+    /* Get method for getMilesValue */
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
-
 }
