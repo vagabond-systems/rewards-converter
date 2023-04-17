77b5cbf6c3d8358522247acb4d71d39d78d499e2
Katherine <80862932+katherinek123@users.noreply.github.com>
2023-04-16 17:16:59 -0700

    commit to branch

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    public static final double conversionRate = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue * conversionRate;
+    }
+    public RewardValue(int milesValue) {
+        this.cashValue = milesValue / conversionRate;
+        this.milesValue = milesValue;
+    }
+    public double getCashValue() {
+        return this.cashValue;
+    }
+    public double getMilesValue() {
+        return this.milesValue;
+    }
 }
