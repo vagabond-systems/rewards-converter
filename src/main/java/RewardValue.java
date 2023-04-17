4d269accc635be70448395c78d91e4152547e9cc
Prachi <kothawade.prachi12@gmail.com>
2023-04-17 17:16:00 -0500

    Updated: Added two methods for conversion of cash to miles and miles to cash.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,29 @@
 public class RewardValue {
-    double cashValue;
-   int milesValue;
-    public RewardValue(double cashValue)
-    {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
-    public RewardValue(int milesValue)
-    {
-        this.milesValue= milesValue;
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public int getMilesValue() {
-        return (int) (this.cashValue / 0.0035);
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 
 }
