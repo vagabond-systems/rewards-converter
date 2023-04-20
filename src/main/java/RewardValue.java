3219f98e8d8673c9e68f5bd23fb0d8775c646780
Pzzles <125101956+Pzzles@users.noreply.github.com>
2023-04-21 01:47:10 +0200

    Update RewardValue.java
    made CONVERSION_RATE constant.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,18 @@
 public class RewardValue {
     private double cashValue;
+    private static final double CONVERSION_RATE = 0.0035;
     private int milesValue;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035);
+        this.milesValue = (int) (cashValue / CONVERSION_RATE);
     }
     
     //It's not possible to have multiple constructors with the same signature.
     //Although airline miles could be a type of currency, I had to use them as a value of type "int".
     public RewardValue(int milesValue){
         this.milesValue = milesValue;
-        this.cashValue = cashValue * 0.0035;
+        this.cashValue = cashValue * CONVERSION_RATE;
     }
 
     public double getCashValue() {
