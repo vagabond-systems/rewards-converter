a4ccbb8ecd96bee838fbe4ec6d2686d0433a2763
crlanier22 <crlanier22@gmail.com>
2023-04-20 00:35:31 -0700

    created constructor and getters for cash and miles

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+public class RewardValue {
+
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
