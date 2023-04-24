664fb51b7f00279c2caaccd6ca683cfcf86e9cd1
Mahiyat <jucse29.361@gmail.com>
2023-04-24 15:53:01 +0600

    modifications made in rewardvalue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -12,12 +12,23 @@ public class RewardValue
        m=mValue;
        c=m*0.0035;
    }
+   public long convertToMiles(double cValue)
+   {
+       m=(long)(cValue/0.0035);
+       return m;
+   }
+
+   public double convertToCash(long mValue)
+   {
+       c=mValue*0.0035;
+       return c;
+   }
    public long getMilesValue()
    {
-       return m;
+       return convertToMiles(this.c);
    }
    public double getCashValue()
    {
-       return c;
+       return convertToCash(this.m);
    }
 }
