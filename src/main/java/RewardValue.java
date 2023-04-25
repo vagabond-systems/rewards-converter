bf83a6611c591115693832957cfff910f82f3692
Imani Mwamlima <imwamlima@gmail.com>
2023-04-25 12:22:18 -0400

    create RewardValue class and test if program runs successfully

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Returns the cash value of the RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Returns how many miles the RewardValue is worth
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
