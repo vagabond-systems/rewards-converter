77635ad934891bbb86ceef4bf92993741e0bfd18
Pzzles <125101956+Pzzles@users.noreply.github.com>
2023-04-25 15:31:25 +0200

    Implemented unit tests for conversion methods

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        double expectedMilesValue = (int)(cashValue / 0.0035);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue(), 0.001);
     }
+
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        double expectedCashValue = milesValue * 0.0035;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue(), 0.001);
     }
 }
