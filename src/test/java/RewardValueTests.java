c51bf83b0e8768e68a8d5efed251b2ae293d6886
Ayla Demiray <aylademiray1@gmail.com>
2023-04-25 19:08:47 -0400

    Added a two JUnit tests for missing test cases for the code of converting cash to miles and converting miles to cash

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,9 +1,10 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class RewardValueTests {
-
+    private final double Airline = 0.001;
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +21,22 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+
+        double cashValue = 100.0;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        double milesValue = rewardValue.getMilesValue();
+        assertFalse(Math.abs(cashValue / 0.0035 - milesValue) > Airline);
+
     }
 
     @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+    void convert_from_miles_to_cash()   {     int milesValue = 10000;
+    RewardValue rewardValue = new RewardValue(milesValue);
+
+    // Act
+    double cashValue = rewardValue.getCashValue();
+
+    // Assert
+    assertFalse(Math.abs(milesValue * 0.0035 - cashValue) > Airline);
     }
 }
