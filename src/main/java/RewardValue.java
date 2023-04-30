176186830fcbc53782a4f9d60b75b20204854ac1
YousefAbuadas <sparkburner@gmail.com>
2023-04-30 11:30:07 -0500

    fixed some issues with Reward Value class not converting correctly
    Now can convert between miles to cash and vice versa simultaneously

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,19 +1,33 @@
 public class RewardValue {
     private double cash;
     private double miles;
+    private static final double RATE = 0.0035;
 
     RewardValue(double cash)
     {
         this.cash=cash;
+        convertToMiles(cash);
+    }
+    RewardValue(int miles)
+    {
+        this.miles= miles;
+        convertToCash(miles);
+    }
+    private void convertToCash(int miles)
+    {
+        this.cash= RATE * ((double) miles);
     }
 
-    public double getCashValue(){
+    private void convertToMiles(double cash)
+    {
+        this.miles = cash / RATE;
+    }
+    public double getCashValue()
+    {
         return cash;
     }
-    public double getMilesValue(){
-        miles = 0.0035 * cash;
-        String s = String.format("%.4f", miles);
-        double d = Double.parseDouble(s);
-        return d;
+    public double getMilesValue()
+    {
+        return miles;
     }
 }
