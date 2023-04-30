1a4a8d2db9c8e9bed4711056c29b953302ed007d
YousefAbuadas <sparkburner@gmail.com>
2023-04-29 23:22:53 -0500

    Added RewardValue class that converts cash to miles
    This can be used for airlines to track cost per mile

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cash;
+    private double miles;
+
+    RewardValue(double cash)
+    {
+        this.cash=cash;
+    }
+
+    public double getCashValue(){
+        return cash;
+    }
+    public double getMilesValue(){
+        miles = 0.0035 * cash;
+        String s = String.format("%.4f", miles);
+        double d = Double.parseDouble(s);
+        return d;
+    }
 }
