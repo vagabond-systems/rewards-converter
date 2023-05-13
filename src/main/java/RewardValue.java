a6a4e5104a993f997335a9acb0d52586f2fd2486
Maksymilian Jan Matusiak <m.matusiak@se22.qmul.ac.uk>
2023-05-13 17:02:44 +0000

    Finalised version

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,23 @@
 public class RewardValue {
     private double cashValue;
-    private double milesValue;
+    private int milesValue;
+    private final double miles_to_cash = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue * 0.0035;
+        this.milesValue = (int) (cashValue / miles_to_cash);
+    }
+
+    public RewardValue(int milesValue) {
+       this.milesValue = milesValue;
+       this.cashValue = milesValue * miles_to_cash;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
+    public int getMilesValue() {
         return milesValue;
     }
 }
