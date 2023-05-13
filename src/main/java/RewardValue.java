05b55e0ee391f0c10858d733e3a85b7360643be4
Maksymilian Jan Matusiak <m.matusiak@se22.qmul.ac.uk>
2023-05-13 14:03:07 +0000

    Finalised version

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,17 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
