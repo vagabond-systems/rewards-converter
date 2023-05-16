7cd429f8221349338862912735512ee25baa39f1
Nithin Raj <nithinraj10@hotmail.com>
2023-05-16 21:41:14 +0530

    ➕ RewardValue file added

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    private double cashValue, value;
+    private double milesValue;
+    public static final double miles_to_cash_rate = 0.0035;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue()
+    {
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        milesValue = getCashValue() * miles_to_cash_rate;
+        return milesValue;
+    }
+}
