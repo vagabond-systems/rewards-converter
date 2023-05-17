3b7b116776ed833d846b01ca309c26af855f6cd4
Nithin Raj <nithinraj10@hotmail.com>
2023-05-17 17:06:04 +0530

    ✅ All four test cases passed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,33 @@
 public class RewardValue {
-    private double cashValue, value;
-    private double milesValue;
+    private final double cashValue;
+//    private int milesValue;
     public static final double miles_to_cash_rate = 0.0035;
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
+        this.cashValue = convertToCash(milesValue);
     }
 
+    public int convertToMiles(double cashValue)
+    {
+//        milesValue = (int) (cashValue / miles_to_cash_rate);
+        return (int) (cashValue / miles_to_cash_rate);
+    }
+
+    public double convertToCash(int milesValue)
+    {
+        return milesValue * miles_to_cash_rate;
+    }
     public double getCashValue()
     {
         return cashValue;
     }
 
     public double getMilesValue(){
-        return milesValue;
+//        milesValue = convertToMiles(this.cashValue);
+        return convertToMiles(cashValue);
+//        return milesValue;
     }
 }
