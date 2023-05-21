0420570a920aab86ac6efe8650f5863c26e59abe
Aryan Agrawal <aryan5276coding@gmail.com>
2023-05-21 02:04:15 -0400

    commiting 2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    double cash;
+    double miles;
+
+    public RewardValue(double cashValue){
+        cash = cashValue;
+        miles = cashValue / 0.0035;
+    }
+
+    public RewardValue(int mileValue){
+        miles = mileValue;
+        cash = mileValue * 0.0035;
+
+    }
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public int getMilesValue() {
+        return (int)miles;
+    }
 }
