ca77cf804686d5a70610948f3484658cbbfb60d7
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-22 17:00:03 -0400

    fixed bug - code didnt read value

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,13 +1,18 @@
 public class RewardValue {
-
     private static final double MILES_TO_CASH_RATE = 0.0035;
     private double cashValue;
-
+    private int milesValue;
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
-
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
     public int getMilesValue() {
         return (int) (cashValue / MILES_TO_CASH_RATE);
+    }
+
+    public double getCashValue(){
+        return cashValue;
     }
 }
