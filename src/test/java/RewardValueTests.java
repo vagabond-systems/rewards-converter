6e5dd3a5d3b8254afab5a1e11a9ac7a1e2e6fd78
Balaji Sivakumar <sivakumar.balaji@gmail.com>
2023-05-26 16:06:16 +0530

    RewardValues class added and Unit test added

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,12 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+    
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        //assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
@@ -20,11 +20,19 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+       // assert false;
+       double cashValue = 100;
+       int expectedValueMiles = (int) (cashValue/ RewardValue.MILES_CASH_CONVERTER_VALUE);
+       var rewardValue = new RewardValue(cashValue); 
+       assertEquals(expectedValueMiles,  rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        //assert false;
+        int milesValue = 1000;
+        double excpectedValuesCash = milesValue * RewardValue.MILES_CASH_CONVERTER_VALUE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(excpectedValuesCash, rewardValue.getCashValue());
     }
 }
