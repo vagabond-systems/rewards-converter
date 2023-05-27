fad13f4b2e8e4bcd2bcc03d6ce602324df720541
Harry Franklin <hrfranklin0702@gmail.com>
2023-05-27 14:02:41 +0100

    Working Testing

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int modelMilesValue = (int) (cashValue / 0.0035);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(modelMilesValue, rewardValue.getMilesValue());
     }
-
+    
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double modelCashValue = milesValue * 0.0035;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(modelCashValue, rewardValue.getCashValue());
     }
 }
