bb348bbd741594033bd460d4582f77ec99c4fc27
Cristi Savca <cristisavca44@gmail.com>
2023-05-28 18:03:34 -0400

    RewardsValue class implemented along with constructors and getters

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cash;
+    private double miles;
+
+    public RewardValue(double cashValue) {
+        cash = cashValue;
+        miles = cashValue * 0.0035;
+    }
+
+    public RewardValue(int milesValue){
+        miles = milesValue;
+        cash = milesValue / 0.0035;
+    }
+
+    public double getMilesValue() {
+        return this.miles;
+    }
+
+    public double getCashValue() {
+        return this.cash;
+    }
 }
