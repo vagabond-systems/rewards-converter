60d33fbf558a3f404289a3c00c4fe27cbbc9b47d
NaolGB <naolgbasaye@gmail.com>
2023-05-30 13:44:53 -0400

    test completed, all passed

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        int cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(100/0.0035, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 100;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(100*0.0035, rewardValue.getCashValue());
     }
 }
