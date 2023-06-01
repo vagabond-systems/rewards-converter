18733e4d38436ca4680faff46f9bcc36133604e3
Ahmed Bakry <ahmedbakry024@gmail.com>
2023-06-01 19:26:55 +0300

    Create RewardValue.java

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+
+public class RewardValue {
+    private Double cashValue;
+    private double mileValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int mileValue) {
+        this.mileValue = mileValue;
+         
+    }
+    public Double getMilesValue() {
+        return cashValue / 0.0035;
+    }
+    public Double getCashValue() {
+        return mileValue * 0.0035;
+    }
+
+}
