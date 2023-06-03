8d9baccb73fde4c8d71f479480f1f8bfa0fd3359
Daniel <daniel.arisa@yahoo.com>
2023-06-02 20:45:10 -0500

    RewardsConverter

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -2,19 +2,44 @@ import java.util.Scanner;
 
 public class RewardsConverter {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Credit Card Rewards Converter!");
-        System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
-        double cashValue;
-        try {
-            cashValue = Double.parseDouble(input_value);
-        } catch (NumberFormatException exception) {
-            System.out.println("Could not parse input value as a double, exiting");
-            return;
+        try (var scanner = new Scanner(System.in)) {
+            System.out.println("Welcome to the Credit Card Rewards Converter!");
+            System.out.println("Please enter a cash value to convert to airline miles: ");
+            var input_value = scanner.nextLine();
+            double cashValue;
+            try {
+                cashValue = Double.parseDouble(input_value);
+            } catch (NumberFormatException exception) {
+                System.out.println("Could not parse input value as a double, exiting");
+                return;
+            }
+            System.out.println("converting $" + input_value + " to miles");
+            var rewardsValue = new RewardValue(cashValue);
+            System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
         }
-        System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
-        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
+
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
+
