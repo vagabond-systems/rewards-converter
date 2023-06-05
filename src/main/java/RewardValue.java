ad292a69b7a9f2e81525181a1c0deb797c25f161
Sam Forquer <samforquer@gmail.com>
2023-06-05 13:31:49 -0400

    testing commit

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,5 +1,22 @@
 import java.math.BigDecimal;
 
 public class RewardValue {
+    private BigDecimal cash;
+    private double miles;
 
+    public RewardValue(BigDecimal cash) {
+        this.cash = cash;
+    }
+
+    public RewardValue(double miles){
+        this.miles = miles;
+    }
+
+    public double getMilesValue(){
+        return 0;
+    }
+
+    public BigDecimal getCashValue(){
+        return null;
+    }
 }
