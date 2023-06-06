99f5e3ef237ed0130777cea2a013c50671f925e4
Samforquer <samforquer@gmail.com>
2023-06-06 02:12:57 -0400

    Completed

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -16,6 +16,7 @@ public class RewardsConverter {
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         double miles = rewardsValue.convertFromCashToMiles();
+        // Adding formatting to show 3 decimal places so as not to change the rounding on the back end.
         String formattedMiles = String.format("%.3f", miles);
         System.out.println("$" + input_value + " is worth " + formattedMiles + " miles");
     }
