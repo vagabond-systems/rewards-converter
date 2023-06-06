14e5b762a0684f7fcf7700a1cb8568f5235429a3
donvelas <donv1337@gmail.com>
2023-06-06 14:22:04 -0700

    Added the RewardValue class which handles getting and converting points

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        milesValue = (int) (cashValue / 0.0035);
+        return milesValue;
+    }
+
+}
