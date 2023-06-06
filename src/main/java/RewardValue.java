f9fd279c274e72efb15121261d2bfd64e31c106c
donvelas <donv1337@gmail.com>
2023-06-06 14:36:22 -0700

    Revised my code for the RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,37 @@
 public class RewardValue {
-    private double cashValue;
-    private int milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION = 0.0035;
 
+    // Assigns the cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
+    // Converts the miles value to the cash value
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
+        this.cashValue = convertToCash(milesValue);
     }
 
+    // Converts the cash value to the number of miles
+    public static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION);
+
+    }
+
+    // Converts the cash value to the number of miles
+    public static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION;
+
+    }
+
+    // Returns the cash value of the reward
     public double getCashValue() {
         return cashValue;
     }
 
+    // Returns the number of miles that can be obtained from the cashValue
     public int getMilesValue() {
-        milesValue = (int) (cashValue / 0.0035);
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
 
 }
