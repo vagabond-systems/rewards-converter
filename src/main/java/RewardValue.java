ebe26e8026dc2afd88de80c29cc3438080b1ef6b
Jasmine Daniels <create.jasminedaniels@gmail.com>
2023-06-09 12:29:44 -0400

    move convertCashToMiles() -> getMilesValue()

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -4,7 +4,6 @@ import java.math.RoundingMode;
 public class RewardValue {
 
     private double cashValue;
-    //change to int
     private int miles;
     private BigDecimal decimal;
 
@@ -18,11 +17,9 @@ public class RewardValue {
 
 
     public double getCashValue(){
-        return cashValue;
-    }
-
-        public double convertFromMilesToCash(){
-
+        if ( miles == 0){
+            return cashValue;
+        }
         if (decimal == null){
             BigDecimal temp = BigDecimal.valueOf(0.0);
             setDecimal(temp);
@@ -30,58 +27,47 @@ public class RewardValue {
             double rate =  .35;
             double product = (miles + decimal.doubleValue()) * rate;
 
-            System.out.println("Grab remainder: " + decimal.doubleValue());
-            System.out.println("This is rate: " + rate);
-            System.out.println("This is product: " + product);
-
             BigDecimal bd = BigDecimal.valueOf(product);
             bd = bd.setScale(2, RoundingMode.HALF_UP);
+            setCashValue(bd.doubleValue());
             return bd.doubleValue();
         } else {
-
             double rate =  .35;
             double product = (miles + decimal.doubleValue()) * rate;
 
-            System.out.println("Grab remainder: " + decimal.doubleValue());
-            System.out.println("This is rate: " + rate);
-            System.out.println("This is product: " + product);
-
             BigDecimal bd = BigDecimal.valueOf(product);
             bd = bd.setScale(2, RoundingMode.HALF_UP);
+            setCashValue(bd.doubleValue());
             return bd.doubleValue();
         }
-
     }
 
-//    public double convertFromMilesToCash(){
-//        double newRate = .35 * 100;
-//        double rate =  newRate + 100;
-//        double quotient = 100 / rate;
-//        double product = quotient * miles;
+//        public double convertFromMilesToCash(){
 //
-////        System.out.println("This is newRate: " + newRate);
-////        System.out.println("This is rate: " + rate);
-////        System.out.println("This is quotient: " + quotient);
-////        System.out.println("This is product: " + product);
+//        if (decimal == null){
+//            BigDecimal temp = BigDecimal.valueOf(0.0);
+//            setDecimal(temp);
 //
-//        BigDecimal bd = BigDecimal.valueOf(product * 100);
-//        bd = bd.setScale(2, RoundingMode.HALF_UP);
-//        return bd.doubleValue();
-//    }
-
-//    public double convertFromMilesToCash(){
-//        double rate = miles * 100;
-//        double product = rate * .35;
-//        double difference = rate - product;
+//            double rate =  .35;
+//            double product = (miles + decimal.doubleValue()) * rate;
 //
-////        System.out.println("This is rate: " + rate);
-////        System.out.println("This is product: " + product);
-////        System.out.println("This is difference: " + difference);
+////            System.out.println("Grab remainder: " + decimal.doubleValue());
+////            System.out.println("This is rate: " + rate);
+////            System.out.println("This is product: " + product);
 //
-//        BigDecimal bd = new BigDecimal(String.valueOf(difference));
-//        bd.setScale(2, RoundingMode.HALF_UP);
-//        //System.out.println("This is BD: " + bd.doubleValue());
-//        return bd.doubleValue();
+//            BigDecimal bd = BigDecimal.valueOf(product);
+//            bd = bd.setScale(2, RoundingMode.HALF_UP);
+//            setCashValue(bd.doubleValue());
+//            return bd.doubleValue();
+//        } else {
+//            double rate =  .35;
+//            double product = (miles + decimal.doubleValue()) * rate;
+//
+//            BigDecimal bd = BigDecimal.valueOf(product);
+//            bd = bd.setScale(2, RoundingMode.HALF_UP);
+//            setCashValue(bd.doubleValue());
+//            return bd.doubleValue();
+//        }
 //    }
 
     public void setCashValue(double cashValue){
@@ -89,45 +75,37 @@ public class RewardValue {
     }
 
     public int getMilesValue(){
-        return miles;
-    }
+        if (cashValue == 0.0){
+            return miles;
+        }
 
-    public int convertFromCashToMiles(){
         double rate = .35;
-//        double product = cashValue * 1;
         BigDecimal product = BigDecimal.valueOf(cashValue * 1);
-        //int quotient = (int)(product / rate);
-//        double quotient = product / rate;
         BigDecimal quotient = BigDecimal.valueOf(product.doubleValue() / rate);
-
         BigDecimal remainder = quotient.subtract(BigDecimal.valueOf(quotient.intValue()));
-        //BigDecimal result = BigDecimal.valueOf(quotient);
-        System.out.println("This is product: " + product);
-        System.out.println("This is quotient: " + quotient);
-        System.out.println("This is int quotient: " + quotient.intValue());
-        System.out.println("This is remainder: " + remainder);
 
         setDecimal(remainder);
-
-
+        setMilesValue(quotient.intValue());
         return quotient.intValue();
     }
 
-//    public double convertFromCashToMiles(){
-//        double product = cashValue * 0.35;
-//        double sum = cashValue + product;
-////        int result = (int)(sum / 100);
-//        double result = sum / 100;
+//    public int convertFromCashToMiles(){
+//        double rate = .35;
+////        double product = cashValue * 1;
+////        double quotient = product / rate;
 //
+//        BigDecimal product = BigDecimal.valueOf(cashValue * 1);
+//        BigDecimal quotient = BigDecimal.valueOf(product.doubleValue() / rate);
+//        BigDecimal remainder = quotient.subtract(BigDecimal.valueOf(quotient.intValue()));
 //
-//        System.out.println("This is product: " + product);
-//        System.out.println("This is sum: " + sum);
-//        System.out.println("This is result: " + result);
+////        System.out.println("This is product: " + product);
+////        System.out.println("This is quotient: " + quotient);
+////        System.out.println("This is int quotient: " + quotient.intValue());
+////        System.out.println("This is remainder: " + remainder);
 //
-//
-//        BigDecimal bd = BigDecimal.valueOf(result);
-//        bd = bd.setScale(4, RoundingMode.HALF_UP);
-//        return bd.doubleValue();
+//        setDecimal(remainder);
+//        setMilesValue(quotient.intValue());
+//        return quotient.intValue();
 //    }
 
     public void setDecimal(BigDecimal decimal){
