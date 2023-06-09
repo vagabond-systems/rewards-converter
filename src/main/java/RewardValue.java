9b479936c60732627908cbe53b2fd4cef78c70ef
Md Salman <mhdsalman010@gmail.com>
2023-06-09 21:00:19 +0530

    made changes to RewardValue - fixed the constructor

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,18 +1,5 @@
 public class RewardValue {
 
-    // private double cashValue;
-    // private double mileValue;
-
-    // private static final double CONVERSION_RATE = 0.0035;
-    // public RewardValue(double cashValue){
-    // this.cashValue = cashValue;
-    // }
-
-    // public RewardValue(double mileValue){
-    // this.mileValue = mileValue;
-
-    // }
-
     private double cashValue;
     private double milesValue;
 
@@ -21,7 +8,7 @@ public class RewardValue {
         this.milesValue = cashValue / 0.0035;
     }
 
-    public RewardValue(double milesValue, boolean isMiles) {
+    public RewardValue(int milesValue) {
         this.milesValue = milesValue;
         this.cashValue = milesValue * 0.0035;
     }
