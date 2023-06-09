cb0e0479deea523f63743ee2cf38d8e645686efd
Jasmine Daniels <create.jasminedaniels@gmail.com>
2023-06-09 12:34:37 -0400

    convertMilesToCash() -> getCashValue(), clean code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -42,34 +42,6 @@ public class RewardValue {
         }
     }
 
-//        public double convertFromMilesToCash(){
-//
-//        if (decimal == null){
-//            BigDecimal temp = BigDecimal.valueOf(0.0);
-//            setDecimal(temp);
-//
-//            double rate =  .35;
-//            double product = (miles + decimal.doubleValue()) * rate;
-//
-////            System.out.println("Grab remainder: " + decimal.doubleValue());
-////            System.out.println("This is rate: " + rate);
-////            System.out.println("This is product: " + product);
-//
-//            BigDecimal bd = BigDecimal.valueOf(product);
-//            bd = bd.setScale(2, RoundingMode.HALF_UP);
-//            setCashValue(bd.doubleValue());
-//            return bd.doubleValue();
-//        } else {
-//            double rate =  .35;
-//            double product = (miles + decimal.doubleValue()) * rate;
-//
-//            BigDecimal bd = BigDecimal.valueOf(product);
-//            bd = bd.setScale(2, RoundingMode.HALF_UP);
-//            setCashValue(bd.doubleValue());
-//            return bd.doubleValue();
-//        }
-//    }
-
     public void setCashValue(double cashValue){
         this.cashValue = cashValue;
     }
@@ -88,25 +60,6 @@ public class RewardValue {
         setMilesValue(quotient.intValue());
         return quotient.intValue();
     }
-
-//    public int convertFromCashToMiles(){
-//        double rate = .35;
-////        double product = cashValue * 1;
-////        double quotient = product / rate;
-//
-//        BigDecimal product = BigDecimal.valueOf(cashValue * 1);
-//        BigDecimal quotient = BigDecimal.valueOf(product.doubleValue() / rate);
-//        BigDecimal remainder = quotient.subtract(BigDecimal.valueOf(quotient.intValue()));
-//
-////        System.out.println("This is product: " + product);
-////        System.out.println("This is quotient: " + quotient);
-////        System.out.println("This is int quotient: " + quotient.intValue());
-////        System.out.println("This is remainder: " + remainder);
-//
-//        setDecimal(remainder);
-//        setMilesValue(quotient.intValue());
-//        return quotient.intValue();
-//    }
 
     public void setDecimal(BigDecimal decimal){
         this.decimal = decimal;
--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -7,7 +7,6 @@ public class RewardsConverter {
         System.out.println("Please enter a cash value to convert to airline miles: ");
         var input_value = scanner.nextLine();
         double cashValue;
-        int milesValue = 282;
         try {
             cashValue = Double.parseDouble(input_value);
         } catch (NumberFormatException exception) {
@@ -16,19 +15,10 @@ public class RewardsConverter {
         }
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
-        //rewardsValue.setCashValue(cashValue);
-        //System.out.println("$" + input_value + " is worth " + rewardsValue.convertFromCashToMiles() + " miles");
-
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
         System.out.println("___________________________________");
-        //rewardsValue.setMilesValue(rewardsValue.convertFromCashToMiles());
         System.out.println("converting " + rewardsValue.getMilesValue() + " miles to cash");
         System.out.println("Your cash rewards value for " + rewardsValue.getMilesValue() + " miles is: $" + rewardsValue.getCashValue() );
-
-        System.out.println("___________________________");
-        var tester = new RewardValue(milesValue);
-       System.out.println(tester.getMilesValue());
-        System.out.println(tester.getCashValue());
 
     }
 }
