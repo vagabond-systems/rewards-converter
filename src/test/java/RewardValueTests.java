aebc396df6488043b735603d926c48c74a31e66b
Akarsh Agarwal <akarshagarwal9863@gmail.com>
2023-06-13 17:24:09 +0530

    done the task2

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 350;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedValue = cashValue/0.0035;
+        assertEquals(expectedValue,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue  = new RewardValue(milesValue);
+        int expectedValue = (int) (milesValue * 0.0035);
+        assertEquals(expectedValue,rewardValue.getCashValue());
     }
 }
