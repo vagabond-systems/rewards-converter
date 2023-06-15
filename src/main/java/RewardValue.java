24fddeb129387fd8ea55c47ba89fa4fb105961a4
Jonas Moloto <jonasmoloto@outlook.com>
2023-06-15 15:59:04 +0200

    init Reward Value class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        double milesToCashRate =  0.0035;
+        return this.cashValue *  milesToCashRate;
+    }
 }
