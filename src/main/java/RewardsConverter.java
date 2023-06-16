c896e19f7d99300a02b59e8941ce460fb6257241
Nick McCullough <nickmcc@iastate.edu>
2023-06-15 23:42:00 -0500

    completed JPMC Forage challenge

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
 
 public class RewardsConverter {
-    public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Credit Card Rewards Converter!");
-        System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
-        double cashValue;
-        try {
-            cashValue = Double.parseDouble(input_value);
-        } catch (NumberFormatException exception) {
-            System.out.println("Could not parse input value as a double, exiting");
-            return;
-        }
-        System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
-        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
-    }
+	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
+		System.out.println("Welcome to the Credit Card Rewards Converter!");
+		System.out.print("Please enter a cash value to convert to airline miles: $");
+		var input_value = scanner.nextLine();
+		double cashValue;
+		try {
+			cashValue = Double.parseDouble(input_value);
+		} catch (NumberFormatException exception) {
+			System.out.println("Could not parse input value as a double, exiting");
+			return;
+		}
+		System.out.println("converting $" + input_value + " to miles");
+		double cashToMiles = RewardValue.convertFromCashToMiles(cashValue); // conversion method from RewardValue class
+		String formatMilesToThreeDecimals = String.format("%.3f", cashToMiles);
+		System.out.println("Cash value entered: $" + input_value + ".");
+		System.out.println("$" + input_value + " is worth " + formatMilesToThreeDecimals + " miles.");
+		System.out.println("Thank you for choosing JPMorgan Chase, have a great day.");
+	}
 }
+
+/*
+EXAMPLE OUTPUT:
+
+Welcome to the Credit Card Rewards Converter!
+Please enter a cash value to convert to airline miles: $1799.00
+converting $1799.00 to miles
+Cash value entered: $1799.00.
+$1799.00 is worth 514000.000 miles.
+Thank you for choosing JPMorgan Chase, have a great day.
+ */
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,37 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        var RV = new RewardValue(cashValue);
+        assertEquals(cashValue, RV.getCashValue());
     }
-
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        var RV = new RewardValue(milesValue);
+        assertEquals(milesValue, RV.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
         assert false;
+        double cash = 10000;
+        double conversion = cash * 0.0035;
+        var RV = new RewardValue(cash);
+        var convertToMiles = RV.convertFromCashToMiles();
+        assertEquals(conversion, convertToMiles);
     }
 
     @Test
     void convert_from_miles_to_cash() {
         assert false;
+        int miles = 35;
+        double conversion = miles / 0.0035;
+        var RV = new RewardValue(miles);
+        var convertToCash = RV.convertFromMilesToCash();
+        assertEquals(conversion, convertToCash);
     }
 }
