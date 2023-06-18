5358cd43648fb7cfbf58d2676d1e963d56f869a2
Yousef Hassanein <yhassanein2001@gmail.com>
2023-06-17 23:46:27 -0400

    Fixed the testing

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,19 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-     private double miles;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
-         this.cashValue = cashValue;
+        this.cashValue = cashValue;
     }
 
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
 
-    public double getCashValue(double cashValue) {
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        miles= cashValue* 0.0035;
-        return miles;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
-    
 }
