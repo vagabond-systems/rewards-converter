b95402c839576b43ad31556881754aafa1457674
akhilesh.nevatia@gmail.com <akhilesh.nevatia@gmail.com>
2023-06-19 00:09:10 -0500

    Organizing code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -5,19 +5,21 @@ public class RewardValue {
 
     private double miles;
 
+    private final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
 
     //Constructor 1 - converts cash given to equivalent miles
     // Also instantiates local instance variables
     public RewardValue(double cash){
         this.cash = cash;
-        miles = cash/0.0035;
+        miles = cash/(MILES_TO_CASH_CONVERSION_RATE);
     }
 
     //Constructor 2 - converts miles given to equivalent cash
     // Also instantiates local instance variables
     public RewardValue(int miles){
         this.miles = miles;
-        cash = miles*0.0035;
+        cash = miles*MILES_TO_CASH_CONVERSION_RATE;
     }
 
   //returns cash value
