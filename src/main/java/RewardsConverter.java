fb17ad9b46877153bc0106f01b775ccc156b7f3c
SamuelPetrucci <Sam.pet8803@yahoo.com>
2023-06-19 13:03:25 -0400

    The RewardValue.java Class completes the logic for converting cashvalue to miles value.

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
+        
         var input_value = scanner.nextLine();
         double cashValue;
+        scanner.close();
         try {
             cashValue = Double.parseDouble(input_value);
         } catch (NumberFormatException exception) {
