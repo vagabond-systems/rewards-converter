9e44211df190fe8a62e08457e1cb8f14f84a7ea2
akhilesh.nevatia@gmail.com <akhilesh.nevatia@gmail.com>
2023-06-19 00:05:07 -0500

    Adding comments to code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,31 @@
 public class RewardValue {
 
+    //Creating private instance variables
     private double cash;
 
     private double miles;
 
-    
+
+    //Constructor 1 - converts cash given to equivalent miles
+    // Also instantiates local instance variables
     public RewardValue(double cash){
         this.cash = cash;
         miles = cash/0.0035;
     }
 
+    //Constructor 2 - converts miles given to equivalent cash
+    // Also instantiates local instance variables
     public RewardValue(int miles){
         this.miles = miles;
         cash = miles*0.0035;
     }
 
+  //returns cash value
   public double getCashValue(){
         return cash;
   }
 
+  //returns miles value
   public double getMilesValue(){
         return miles;
   }
