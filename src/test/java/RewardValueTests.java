f3cbeccbfa9b815924e273f0f3ae41e99ae501f0
JW <114599894+judewashock518@users.noreply.github.com>
2023-06-19 17:28:52 -0400

    created two passing unit tests converting miles to cash + cash to miles

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -2,6 +2,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class RewardValue {
+    private double cashValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        double milesValue = cashValue / 0.0035;
+        return milesValue;
+    }
+}
+
 public class RewardValueTests {
 
     @Test
@@ -13,18 +30,25 @@ public class RewardValueTests {
 
     @Test
     void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        double milesValue = 10000.0;
+        var rewardValue = new RewardValue(milesValue * 0.0035);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMilesValue = cashValue / 0.0035;
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue * 0.0035);
+        double expectedCashValue = milesValue * 0.0035;
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
+
     }
 }
