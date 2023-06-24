6c92d74587ba54dc5d4fc04198c6eb2a517c8615
David Swanstrom <ifritborndesigns@gmail.com>
2023-06-24 14:27:22 -0600

    tested cash to miles and miles to cash, use of double may cause rounding issues, suggest using BigDecimal instead

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -9,6 +9,9 @@ public class RewardsConverter {
         System.out.println("Please enter a cash value to convert to airline miles: ");
         var input_value = scanner.nextLine();
         double cashValue;
+        int milesValue;
+
+
 
         try {
             cashValue = Double.parseDouble(input_value);
@@ -20,5 +23,20 @@ public class RewardsConverter {
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+
+        System.out.println("Convert Miles to Cash value. Input miles.");
+        var miles_value = scanner.nextLine();
+
+        try {
+            milesValue = Integer.parseInt(miles_value);
+        }catch (NumberFormatException exception){
+            System.out.println("Could not parse input to int, exiting");
+            return;
+        }
+        System.out.println("converting " + milesValue + " Miles to cash value.");
+        var convertedToCash = new RewardValue(milesValue);
+
+        System.out.println(milesValue + "miles is worth $" + convertedToCash.getCashValue());
+
     }
 }
