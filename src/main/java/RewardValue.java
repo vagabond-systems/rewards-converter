e197e4709563baf6d136b6edccd07b7c32a08077
sk1101 <skjan2@gmail.com>
2023-06-24 23:35:40 +0530

    final answer

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    double cashValue;
+    int miles;
+    public RewardValue(double cv) {
+        this.cashValue = cv;
+    }
+    public RewardValue(int ms) {
+        this.miles = ms;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    public int getMilesValue(){
+        int answer = (int)(this.cashValue/0.0035);
+        return answer;
+    }
 }
