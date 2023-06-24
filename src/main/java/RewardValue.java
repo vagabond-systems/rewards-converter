c88f9bf8d64af8ef0cdca1229c5993219bd76b8a
sk1101 <skjan2@gmail.com>
2023-06-24 23:54:00 +0530

    added test cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,19 +1,26 @@
 public class RewardValue {
     double cashValue;
-    int miles;
     public RewardValue(double cv) {
         this.cashValue = cv;
     }
     public RewardValue(int ms) {
-        this.miles = ms;
+        this.cashValue = convertToCash(ms);
     }
 
     public double getCashValue() {
         return this.cashValue;
     }
 
+    private int convertToMiles(double c){
+        int mile = (int)(c/0.0035);
+        return mile;
+    }
+
+    private double convertToCash(int m){
+        double cash = m*0.0035;
+        return cash;
+    }
     public int getMilesValue(){
-        int answer = (int)(this.cashValue/0.0035);
-        return answer;
+        return convertToMiles(this.cashValue);
     }
 }
