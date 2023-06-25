a4c39baa15659b868b0489ef6741c5a9f24276ce
GargDipesh <dipeshgarg423@gmail.com>
2023-06-25 18:11:15 +0530

    Test

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,13 +1,24 @@
 public class RewardValue {
 
     private double cashValue;
+    private int milesValue;
     private double convesionFactor= 0.0035;
 
     public RewardValue(double cashValue){
         this.cashValue=cashValue;
+        this.milesValue=(int) cashValue/convesionFactor;
     }
 
-    public double getMilesValue(){
-        return this.cashValue/convesionFactor;
+    public RewardValue(int milesValue){
+        this.milesValue=milesValue;
+        this.cashValue= milesValue * convesionFactor;
+    }
+
+    public int getMilesValue(){
+        return this.milesValue;
+    }
+
+    public double getCashValue(){
+        return  this.cashValue;
     }
 }
