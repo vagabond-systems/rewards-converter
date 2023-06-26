84df04761e1ed636e71f12297d2c446c7f5714e2
Michael Kim <michael.dev.kim@gmail.com>
2023-06-26 16:29:24 -0700

    created Reward Value class and Revward Value constructor and methods.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,23 @@
 public class RewardValue {
+    private final double CASH_TO_MILES_CONVERSION_RATE = 0.0035;
+    private double cashValue;
+    private int miles;
 
     public RewardValue(double cashValue) {
-
+        this.cashValue = cashValue;
+        this.miles = (int)(cashValue / CASH_TO_MILES_CONVERSION_RATE);
     }
 
     public RewardValue(int miles) {
-
+        this.miles = miles;
+        this.cashValue = miles * CASH_TO_MILES_CONVERSION_RATE;
     }
 
     public Double getCashValue() {
-        return null;
+        return cashValue;
     }
 
-    public String getMilesValue() {
-        return null;
+    public int getMilesValue() {
+        return miles;
     }
-
-
 }
