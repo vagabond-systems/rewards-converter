7f7c1cf236a5927fb75aef9a423e13a20b7c2400
Michael Kim <michael.dev.kim@gmail.com>
2023-06-26 15:57:38 -0700

    modified pom.xml to change compiler compliance to JRE 20.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,15 +1,20 @@
-
 public class RewardValue {
 
     public RewardValue(double cashValue) {
+
+    }
+
+    public RewardValue(int miles) {
+
+    }
+
+    public Double getCashValue() {
+        return null;
     }
 
     public String getMilesValue() {
         return null;
     }
 
-    public Double getCashValue() {
-        return null;
-    }
 
 }
