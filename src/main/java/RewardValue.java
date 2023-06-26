78c8c2c5ffbc7044b922c5398e94f11c3d8157c3
Michael Kim <michael.dev.kim@gmail.com>
2023-06-26 15:39:51 -0700

    first commit - created reward-value branch with base code

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,15 @@
+
+public class RewardValue {
+
+    public RewardValue(double cashValue) {
+    }
+
+    public String getMilesValue() {
+        return null;
+    }
+
+    public Double getCashValue() {
+        return null;
+    }
+
+}
