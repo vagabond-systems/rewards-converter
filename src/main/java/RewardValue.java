e0c209403b421f63ef00d333596215ab3c37c6a1
Michael Kim <michael.dev.kim@gmail.com>
2023-06-26 17:15:52 -0700

    changed variable name from CASH_TO_MILES_CONVERSION_RATE to MILES_TO_CASH_CONVERSION_RATE. Added JavaTest Values from SampleAnswer repo

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,21 @@
 public class RewardValue {
 
-    private final double cashValue;
+    // Is this conversion value correct? Seems like a ridiculous Conversion Rate
+    // $1000 -> 285,714 miles
+    // 1000 miles -> $3.5
+    
     public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    private double cashValue;
+    private int miles;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+        this.miles = (int)(cashValue / MILES_TO_CASH_CONVERSION_RATE);
     }
 
-    public RewardValue(int milesValue) {
-        this.cashValue = convertToCash(milesValue);
-    }
-
-    private static int convertToMiles(double cashValue) {
-        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
-    }
-
-    private static double convertToCash(int milesValue) {
-        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    public RewardValue(int miles) {
+        this.miles = miles;
+        this.cashValue = miles * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
@@ -24,32 +23,6 @@ public class RewardValue {
     }
 
     public int getMilesValue() {
-        return convertToMiles(this.cashValue);
+        return miles;
     }
-
-    // // Is this conversion value correct? Seems like a ridiculous Conversion Rate
-    // // $1000 -> 285,714 miles
-    // // 1000 miles -> $3.5
-
-    // private final double CASH_TO_MILES_CONVERSION_RATE = 0.0035;
-    // private double cashValue;
-    // private int miles;
-
-    // public RewardValue(double cashValue) {
-    // this.cashValue = cashValue;
-    // this.miles = (int)(cashValue / CASH_TO_MILES_CONVERSION_RATE);
-    // }
-
-    // public RewardValue(int miles) {
-    // this.miles = miles;
-    // this.cashValue = miles * CASH_TO_MILES_CONVERSION_RATE;
-    // }
-
-    // public double getCashValue() {
-    // return cashValue;
-    // }
-
-    // public int getMilesValue() {
-    // return miles;
-    // }
 }
