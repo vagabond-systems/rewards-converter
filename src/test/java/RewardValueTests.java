267a6a0b586d8e34603eb698c1ad8d96c2e17656
Goura Koti <koti.goura@gmail.com>
2023-06-27 19:25:34 +0100

    Task2 Success

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -21,15 +21,16 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
         var rewardValue = new RewardValue(cashValue);
-        assertEquals((int) (cashValue/ MILES_TO_CASH_CONVERSION_RATE), rewardValue.convert_to_miles(cashValue));
-
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
+        double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
         var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue * MILES_TO_CASH_CONVERSION_RATE, rewardValue.convert_to_cash(milesValue));
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
