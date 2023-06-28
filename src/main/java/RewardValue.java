5388932ef2fa3afb01bdc66da3c9c9c9abbc321a
Anuj Kumar <Anujk00@hotmail.com>
2023-06-27 19:22:35 -0700

    Created a class that turns miles into cash and vice versa

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,20 @@
+public class RewardValue {
+    private double cashValue;
+    private int miles;
+    public RewardValue(double cashValue){
+        this.cashValue =cashValue;
+        this.miles= (int)(cashValue * 0.0035);
+    }
+    public RewardValue(int miles){
+        this.miles= miles;
+        this.cashValue=(miles / 0.0035);
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    public int getMilesValue() {
+        return this.miles;
+    }
+}
