4d4af6eabb833a816dfd432a09b6ff72d6fce1a0
Anuj Kumar <Anujk00@hotmail.com>
2023-06-27 20:01:06 -0700

    Put the correct forumla

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,11 +3,11 @@ public class RewardValue {
     private int miles;
     public RewardValue(double cashValue){
         this.cashValue =cashValue;
-        this.miles= (int)(cashValue * 0.0035);
+        this.miles= (int)(cashValue / 0.0035);
     }
     public RewardValue(int miles){
         this.miles= miles;
-        this.cashValue=(miles / 0.0035);
+        this.cashValue=(miles * 0.0035);
     }
 
     public double getCashValue() {
