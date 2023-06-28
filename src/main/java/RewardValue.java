778bee08e08ed7acb3f7fd1a5237478096ed16b2
Simon Zhang <zhangshi0512@gmail.com>
2023-06-27 22:53:02 -0700

    initial commit

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    private double cash;
+    private double miles;
+
+    public RewardValue(double cash){
+        this.cash = cash;
+        this.miles = cash / 0.0035;
+    }
+
+    public RewardValue(int miles){
+        this.miles = miles;
+        this.cash = miles * 0.0035;
+    }
+
+    public double getCashValue(){
+        return cash;
+    }
+
+    public double getMilesValue(){
+        return miles;
+    }
+}
