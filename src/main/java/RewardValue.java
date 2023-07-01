57c825ac5dd6342aa9d2b9a61529aaf73216662a
matthieu <decker.matthieu@gmail.com>
2023-07-01 11:27:07 -0400

    part 1 finished, edited

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,26 +1,26 @@
 public class RewardValue {
     private double cashValue=0;
-    private long milesValue=0;
+    private int milesValue=0;
     final double MILES_TO_CASH_RATE = .0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
-    public RewardValue(long milesValue) {
+    public RewardValue(int milesValue) {
         this.milesValue=milesValue;
     }
 
     public double getCashValue(){
-        if(milesValue!=0){
-            cashValue = milesValue/MILES_TO_CASH_RATE;
-        }
         return cashValue;
     }
 
-    public long getMilesValue(){
+    public int getMilesValue(){
         if(cashValue!=0){
-            milesValue = (long) (cashValue* MILES_TO_CASH_RATE);
+            milesValue = convertCashToMiles(cashValue);
         }
         return milesValue;
+    }
+    private int convertCashToMiles(double cashValue){
+        return (int) (cashValue* MILES_TO_CASH_RATE);
     }
 }
