ad9d20b18df110baf605dd744f16bf16b643cde3
Suvankar Das <suvankard789@gmail.com>
2023-07-02 02:54:02 +0530

    Test Suit completed & passed

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -14,17 +14,21 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        var rewardValue = new RewardValue(milesValue,true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double milesValue = 3571428.5714285714;
+        var rewardValue = new RewardValue(12500);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double cashValue = 1241.2575;
+        var rewardValue = new RewardValue(354645,true);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 }
