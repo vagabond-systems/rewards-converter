323c5c4b8f1137d3465e3b49e88ee2f281082e9f
Jordan McFall <jkmcfall17@gmail.com>
2023-07-03 13:24:31 -0400

    made RewardValue class with two constructors one for double cash and the other for int miles

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,13 @@
+public class RewardValue {
+    double cashValue;
+    int mileValue;
+
+    public RewardValue(double cash){
+        cashValue = cash;
+    }
+
+    public RewardValue(int miles){
+        int mileValue = miles;
+    }
+
+}
