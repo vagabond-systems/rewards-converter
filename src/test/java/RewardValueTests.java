24a1f133a678191f3c3099ba5ba00222b30ba2df
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2023-07-04 12:20:07 -0500

    tests added - all tests passing

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
+    private static final double MILES_TO_CASH_RATE = 0.0035;
 
     @Test
     void create_with_cash_value() {
@@ -20,11 +21,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMilesValue = (int)(cashValue / MILES_TO_CASH_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        double expectedCashValue = milesValue * MILES_TO_CASH_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue,rewardValue.getCashValue());
     }
 }
