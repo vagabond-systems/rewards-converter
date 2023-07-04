9025f414df877f768a1a7bcbfe6cdd4a656e9f27
smitstroy <troyjsmits@gmail.com>
2023-07-04 14:11:28 -0400

    Clean up code for final version of app - values set within Constructors instead of Getters

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -8,26 +8,21 @@ public class RewardValue {
     // Constructors
     public RewardValue(double cash) {
         this.cash = cash;
+        this.miles = cash * CONVERSION_RATE;
     }
 
     public RewardValue(int miles) {
         this.miles = miles;
+        this.cash = miles / CONVERSION_RATE;
     }
 
-    // Public methods
+    // Getters
     public double getCashValue() {
-        if (cash == 0) {
-            cash = miles / CONVERSION_RATE;
-        }
         return (double) (Math.round(cash * 100.0) / 100.0);
     }
 
     public double getMilesValue() {
-        if (miles == 0) {
-            miles = cash * CONVERSION_RATE;
-        }
         return (double) (Math.round(miles * 100.0) / 100.0);
     }
-
 
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
