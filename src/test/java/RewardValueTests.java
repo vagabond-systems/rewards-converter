997a466bdf0554f9fd9104fedcca7927510e71ba
bustamantechris99 <bustamante.chris99@gmail.com>
2023-07-05 00:28:49 -0400

    test working

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -12,7 +12,28 @@ public class RewardValueTests {
     }
 
     @Test
-    void create_with_miles_value() {
+    void create_with_miles_value() {@Test
+        void create_with_cash_value() {
+            double cashValue = 100;
+            var rewardValue = new RewardValue(cashValue);
+            assertEquals(cashValue, rewardValue.getCashValue());
+        }
+
+        @Test
+        void create_with_miles_value() {
+            int milesValue = 10000;
+            var rewardValue = new RewardValue(milesValue);
+            assertEquals(milesValue, rewardValue.getMilesValue());
+        }
+
+        @Test
+        void convert_from_cash_to_miles() {
+            double cash = 30.00;
+            int miles = (int)(cash / 0.0035);
+            var rewardValue = new RewardValue(cash);
+            assertEquals(miles, rewardValue.convertToMiles(cash));
+
+        }
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
@@ -20,11 +41,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cash = 30.00;
+        int miles = (int)(cash / 0.0035);
+        var rewardValue = new RewardValue(cash);
+        assertEquals(miles, rewardValue.convertToMiles(cash));
+
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int miles = 1000;
+        double cash = (miles * 0.0035);
+        var rewardValue = new RewardValue(miles);
+        assertEquals(cash, rewardValue.convertToCash(miles));
     }
 }
