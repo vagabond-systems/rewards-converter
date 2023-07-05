c9dffc12773d8c8543b84c4703d3ffc89b27d407
Chira Levy <chiralevy@gmail.com>
2023-07-05 17:28:29 -0400

    Created a test suite for the RewardValue class

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -18,13 +18,21 @@ public class RewardValueTests {
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
+    // Test the ability of the RewardValue class to convert from cash to miles
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 250;
+        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
+    // Test the ability of the RewardValue class to convert from miles to cash
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 25000;
+        double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
