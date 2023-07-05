c80eb68dbbba553721d56f1e8013cb8498613098
James <james.d.trussell@gmail.com>
2023-07-04 21:16:59 -0500

    Modified test of convertfrommilestocash to accoun t for precision lost when compare the tested value to the actual value

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -23,7 +23,7 @@ public class RewardValueTests {
     void convert_from_cash_to_miles() {
         double cash = 100.5;
         var rewardValue = new RewardValue(cash);
-        int milesTestValue = (int) (cash/0.0035);
+        int milesTestValue = (int) (cash/0.0035); //due to integer the value is only 28714 due to exclusion of decimals
         assertEquals(milesTestValue, rewardValue.getMilesValue());
     }
 
@@ -31,7 +31,7 @@ public class RewardValueTests {
     void convert_from_miles_to_cash() {
         int miles = 555;
         var rewardValue = new RewardValue(miles);
-        double cashTestValue = miles * 0.0035;
-        assertEquals(cashTestValue, rewardValue.getCashValue());
+        double cashTestValue = miles * 0.0035; //expected value 1.9425
+        assertEquals(cashTestValue, rewardValue.getCashValue(), 0.0001); //Compare two doubles with 0.0001 precision lost
     }
 }
