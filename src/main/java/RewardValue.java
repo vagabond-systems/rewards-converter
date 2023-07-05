4f40f20b70c878a02728611a3090cabc58deb163
sknooyen <sabrinanguyen21@yahoo.com>
2023-07-05 16:20:15 -0400

    created new file RewardValue.java

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,20 @@
+public class RewardValue {
+    double cash;
+    int miles;
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+    }
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public double getMilesValue() {
+        return cash * 0.0035;
+    }
+}
