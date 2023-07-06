b91fdfa97d084001ece9f0586b7b0d4f4118a77d
Dylan Carver <dylancarver14@gmail.com>
2023-07-05 23:19:22 -0500

    Created & implemented logic + added tests

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue * 0.0035;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue / 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    public void convertFromCashToMiles() {
+        milesValue = cashValue * 0.0035;
+    }
+
+    public void convertFromMilesToCash() {
+        cashValue = milesValue / 0.0035;
+    }
+}
