3e5572300d4925a58b308d5382281ea492fa5bdf
choco-mint1 <115313891+choco-mint1@users.noreply.github.com>
2023-07-05 23:00:57 -0500

    Created RewardValue class with design reqs

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class RewardValue {
+    
+    private double cashValue;
+    private float milesValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (float) (cashValue * MILES_TO_CASH_RATE);
+    }
+
+    public RewardValue(float milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue / MILES_TO_CASH_RATE;
+    }
+
+    public double getCashValue() {
+
+        return cashValue;
+    }
+
+    public float getMilesValue() {
+
+        return milesValue;
+    }
+
+}
