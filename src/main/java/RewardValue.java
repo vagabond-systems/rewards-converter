b0af96bca86d9b5a0f3041f18bd89e279b03b980
Kevin Salinas <kevinsalinas155@gmail.com>
2023-07-07 12:47:26 -0400

    add convertomiles and convertocash method

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,29 @@
 public class RewardValue {
 
     private double cash;
-    private int miles;
+
 
     public RewardValue(double cashValue) {
-
         this.cash = cashValue;
-        this.miles = (int) (cashValue / 0.0035);
     }
 
-    public double getCashValue() {
-        return this.cash;
+    public RewardValue(int milesValue) {
+        this.cash = convertToCash(milesValue);
     }
+
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+
+    public double getCashValue() { return this.cash; }
 
     public int getMilesValue() {
-        return this.miles;
+        return convertToMiles(this.cash);
     }
 
 
