701a3ddc1cdf88c9116eac8138297df06f7dca7f
Pranav Agrawal <pranav@pranavs-mbp-2.mynetworksettings.com>
2023-07-07 22:08:58 -0400

    code added for RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class RewardValue {
+/*
+* This class must satisfy the following requirements:
+* RewardValue must have two constructors: one that accepts a cash value and one that accepts a value in miles.
+* RewardValue must have a getCashValue() method, which returns the cash value of the RewardValue.
+* RewardValue must have a getMilesValue() method, which returns how many miles the RewardValue is worth.
+* RewardValue must convert from miles to cash at a rate of 0.0035.
+* */
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    public RewardValue(double cashValue) {
+        this.cashValue=cashValue;
+        this.milesValue=cashValue*0.0035;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue=milesValue;
+        this.cashValue=milesValue/0.0035;
+    }
 }
