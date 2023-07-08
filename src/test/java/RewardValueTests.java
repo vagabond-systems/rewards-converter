4e48a4d10cc57072ffcf5c30c187bab0295eb721
Priyanshu Vedwal <priyanshuvedwal20@gmail.com>
2023-07-08 23:24:50 +0530

    Updated RewardValueTests code

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -2,7 +2,40 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+
+    public int convertToMiles(double conversionRate) {
+        return (int) (cashValue * conversionRate);
+    }
+
+    public double convertToCash(double conversionRate) {
+        return milesValue / conversionRate;
+    }
+}
+
 public class RewardValueTests {
+
+    public RewardValueTests() {
+        // Default constructor
+    }
 
     @Test
     void create_with_cash_value() {
@@ -20,11 +53,19 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 1000;
+        double conversionRate = 0.1; // 1 unit of cash = 0.1 mile
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMilesValue = (int) (cashValue * conversionRate);
+        assertEquals(expectedMilesValue, rewardValue.convertToMiles(conversionRate));
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 350;
+        double conversionRate = 0.1; // 1 mile = 0.1 unit of cash
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue / conversionRate;
+        assertEquals(expectedCashValue, rewardValue.convertToCash(conversionRate));
     }
 }
