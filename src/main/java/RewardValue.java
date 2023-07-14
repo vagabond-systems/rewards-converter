17119a1affac4d7128ff3a6d336baa961dd323dc
amansahu <amansahu0706200@gmail.com>
2023-07-14 23:00:54 +0530

    RewardValue class added

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,27 @@
+public class RewardValue {
+      final   private double cashValue;
+       final   private int milesValue;
+
+        // Constructor that accepts cash value
+        public RewardValue(double cashValue) {
+            this.cashValue = cashValue;
+            this.milesValue = (int) (cashValue / 0.0035);
+        }
+
+        // Constructor that accepts miles value
+        public RewardValue(int milesValue) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * 0.0035;
+        }
+
+        // Method to get the cash value of the RewardValue
+        public double getCashValue() {
+            return cashValue;
+        }
+
+        // Method to get the miles value of the RewardValue
+        public int getMilesValue() {
+            return milesValue;
+        }
+    }
+
