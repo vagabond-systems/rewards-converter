f9bc6fe6742f476aee93d6e184aaca5241b91008
Bruce <bruce.hillsworth@gmail.com>
2023-07-14 21:37:50 -0600

    Add RewardValue class that contains a constructor that accepts a cash value and a constructor that accepts a miles value.  Add getCashValue() and getMilesValue() methods to RewardValue class.  Add convert_from_cash_to_miles() and convert_from_miles_to_cash() to test RewardValue class.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 10000;
+        int expectedMilesValue = (int) (cashValue / 0.0035);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double expectedCashValue = (double) (milesValue * 0.0035);
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
