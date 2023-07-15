a36340b62d1643df01afc986ab58e5568d490f39
danielpdaniel <artofjquinn@gmail.com>
2023-07-15 11:40:19 -0400

    Update convert_from_cash_to_miles to check that cashValue is being properly converted

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -19,7 +19,11 @@ public class RewardValueTests {
     }
 
     @Test
-    void convert_from_cash_to_miles() { assert false; }
+    void convert_from_cash_to_miles() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(28571 , rewardValue.getMilesValue());
+    }
 
     @Test
     void convert_from_miles_to_cash() {
