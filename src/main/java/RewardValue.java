6114a46343fd44e0cfd28ef07f7a9af4ffaead26
DanielP <danielpenalinks@gmail.com>
2023-07-20 10:19:27 -0400

    Update RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    public double cashValue;
+    public int milesValue;
+    private final double CONVERSION_RATE = 0.0035;
+    public RewardValue (double cashInput)
+    {
+        cashValue=cashInput;
+        milesValue= cashToMiles(cashInput);
+    }
+    public RewardValue (int milesInput)
+    {
+        milesValue=milesInput;
+        cashValue= milesToCash(milesInput);
+    }
+    public int getMilesValue()
+    {
+        return milesValue;
+    }
+    public double getCashValue(){
+        return cashValue;
+    }
+    public int cashToMiles(double cash)
+    {
+        return (int)(cash * CONVERSION_RATE);
+    }
+    public double milesToCash(int miles)
+    {
+        return (double) (miles/CONVERSION_RATE);
+    }
 }
