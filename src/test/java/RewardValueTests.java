9c2adbed91427f95c4791ebaff4b79f87fae0371
Diego Feresin <diegoferesin@gmail.com>
2023-07-21 17:42:40 -0300

    Solved challenge

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -7,24 +7,38 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
+
         var rewardValue = new RewardValue(cashValue);
+
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
+
         var rewardValue = new RewardValue(milesValue);
+
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 105;
+        int expectedMiles = (int) (cashValue / RewardValue.CONVERSION_RATE);
+
+        var rewardValue = new RewardValue(cashValue);
+
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 100000;
+        double expectedCashValue = milesValue * RewardValue.CONVERSION_RATE;
+
+        var rewardValue = new RewardValue(milesValue);
+
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
