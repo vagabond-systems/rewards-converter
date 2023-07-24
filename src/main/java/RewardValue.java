04d48fb391f7c602d544a209c1d599f00a7e414a
Ryan <zambier@lafayette.edu>
2023-07-23 21:41:29 -0500

    finished test

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
 
 public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-private double cash;
-private int miles;
-    public RewardValue(double cash) {
-        this.cash = cash;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    public RewardValue(int miles) {
-        this.miles = miles;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
-        return miles * 0.0035;
+        return cashValue;
     }
 
-    public double getMilesValue() {
-        return cash / 0.0035;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
