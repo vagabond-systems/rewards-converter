89f12e3fcf297b69dc0c15e4432a303c277e7c1e
Rohan Bhatt <rohanbhattcs@gmail.com>
2023-07-25 16:47:27 -0400

    finished

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,16 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        System.out.println(rewardValue.getMilesValue());
+        assertEquals(cashValue / 0.0035, rewardValue.getMilesValue()); 
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue * 0.0035, rewardValue.getCashValue());
     }
 }
