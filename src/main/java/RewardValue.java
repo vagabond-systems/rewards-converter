255baa7a58bb4c881518fa818a21e6d65b933021
Matthew Micklin <micklinm12@gmail.com>
2023-07-25 21:28:47 -0400

    Finished Task 2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,14 +3,16 @@ public class RewardValue {
     private double cash;
     private double miles;
 
+    private final double CONVERSION_RATE = 0.0035;
+
     public RewardValue(double cash) {
         this.cash = cash;
-        this.miles = cash / 0.0035;
+        this.miles = cash / CONVERSION_RATE;
     }
 
     public RewardValue(int miles) {
         this.miles = (double) miles;
-        this.cash = miles * 0.0035;
+        this.cash = miles * CONVERSION_RATE;
     }
 
     public double getCashValue() {
@@ -19,5 +21,15 @@ public class RewardValue {
 
     public double getMilesValue() {
         return miles;
+    }
+
+    public double convertCashToMiles() {
+        miles = cash / CONVERSION_RATE;
+        return miles;
+    }
+
+    public double convertMilesToCash() {
+        cash = miles * CONVERSION_RATE;
+        return cash;
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double cashValue = milesValue * 0.0035;
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double milesValue = cashValue / 0.0035;
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 }
