08535a64df523f0d027888840d64a3ffa70cc0ba
Sarika Kulkarni <sarika.kulkarni05@gmail.com>
2023-07-27 22:07:34 -0400

    Updated rewardValueTests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -14,17 +14,25 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        var rewardValue = new RewardValue(milesValue, true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.001);
+        //assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue, true); // Marking the constructor explicitly as miles
+        double expectedCash = milesValue * 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.001);
+        //assert false;
     }
 }
