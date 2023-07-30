69debf77e76b09e73d9499cfa211cec7def2dcb5
Joey <thejosephacevedo@gmail.com>
2023-07-30 12:34:21 -0400

    Completed Unit Testing

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,18 +1,28 @@
 public class RewardValue {
-    double conversionRate = 0.0035;
-    double cash;
-    double miles;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    public RewardValue (double cashValue){
-         cash = cashValue;
-         miles = cashValue / conversionRate;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    public double getCashValue(){
-        return this.cash;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public double getMilesValue(){
-        return this.miles;
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
