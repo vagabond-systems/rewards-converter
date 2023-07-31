51265ca93969df6862cfefd60be40132d27a68ad
mackaysappor <labs.mackay@gmail.com>
2023-07-31 14:36:23 +0000

    convert to cash/miles methods completed

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -14,17 +14,21 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        var rewardValue = new RewardValue(milesValue, true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 35;
+        var reward = new RewardValue(cashValue);
+        assertEquals(reward.getMilesValue(), cashValue/0.0035, String.format("$%s = %s miles.", cashValue, reward.getMilesValue()));
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double mileValue = 10000;
+        var reward = new RewardValue(mileValue, true);
+        assertEquals(reward.getCashValue(), mileValue*0.0035, String.format("%s miles = $%s.", mileValue, reward.getCashValue()));
     }
 }
