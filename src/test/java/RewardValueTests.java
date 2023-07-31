1b84738731285100b3755194d7527f7a179df349
mackaysappor <labs.mackay@gmail.com>
2023-07-31 14:39:11 +0000

    convert to cash/miles methods completed

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    private final double MILES_TO_CASH_RATE = 0.0035;
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -22,13 +23,13 @@ public class RewardValueTests {
     void convert_from_cash_to_miles() {
         double cashValue = 35;
         var reward = new RewardValue(cashValue);
-        assertEquals(reward.getMilesValue(), cashValue/0.0035, String.format("$%s = %s miles.", cashValue, reward.getMilesValue()));
+        assertEquals(reward.getMilesValue(), cashValue/MILES_TO_CASH_RATE, String.format("$%s = %s miles.", cashValue, reward.getMilesValue()));
     }
 
     @Test
     void convert_from_miles_to_cash() {
         double mileValue = 10000;
         var reward = new RewardValue(mileValue, true);
-        assertEquals(reward.getCashValue(), mileValue*0.0035, String.format("%s miles = $%s.", mileValue, reward.getCashValue()));
+        assertEquals(reward.getCashValue(), mileValue*MILES_TO_CASH_RATE, String.format("%s miles = $%s.", mileValue, reward.getCashValue()));
     }
 }
