d5c340d3a6f13aa138cf28efd015b60e9d82d8c5
Luis C.M <42545778+c0508383@users.noreply.github.com>
2023-07-30 21:19:36 -0500

    RewardValueTests: -Implemented unit tests for convert_from_cash_to_miles and convert_from_miles_to_cash.
    RewardValue:
    -Made conversion rate of miles to cash a public static variable.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,16 +1,17 @@
 public class RewardValue {
     private final double cashValue;
+    public static final double MilesConversionRate = 0.0035D;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
     public RewardValue(int miles) {
-        this.cashValue = miles/0.0035D;
+        this.cashValue = miles/MilesConversionRate;
     }
 
     public int getMilesValue() {
-        return (int) Math.floor(this.cashValue * 0.0035D);
+        return (int) Math.floor(this.cashValue * MilesConversionRate);
     }
 
     public double getCashValue() {
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,19 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+
+        int milesValue = (int) Math.floor(cashValue * RewardValue.MilesConversionRate);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+
+        double cashValue = milesValue / RewardValue.MilesConversionRate;
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 }
