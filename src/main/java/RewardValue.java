bcac25c996fa6508909e6d2731323208535d585a
Akshay <akshaypandey.study@gmail.com>
2023-07-31 18:32:34 +0530

    Added RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,25 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(float milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+      public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get how many miles the RewardValue is worth
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
