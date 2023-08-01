68dea0b06bb98f09cda2ba86b5470e407c230cbd
pranavrajesh02 <132791446+pranavrajesh02@users.noreply.github.com>
2023-08-01 13:43:12 -0400

    Add files via upload
    Created 2 Test Cases that manually computes and checks RewardsValue class and see if values match.

---
+++ RewardValueTests.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RewardValueTests {
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_cash_to_miles() {
+        double cashValue = 10000; 
+        int milesValue = (int)(cashValue * 0.0035); 
+        var calculatedMilesValue = new RewardValue(cashValue).getMilesValue(); 
+        assertEquals(milesValue, calculatedMilesValue); 
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+        int milesValue = 10000; 
+        double cashValue = (double)(mileValue / 0.0035); 
+        var calculatedCashValue = new RewardValue(cashValue).getMilesValue(); 
+        assertEquals(cashValue, calculatedCashValue); 
+    }
+}
