14ee28315ee35e5e701ea85a19550bbe03e66926
Aditya-Reniwal <141135104+Aditya-Reniwal@users.noreply.github.com>
2023-08-03 21:55:17 +0530

    Update RewardsConverter.java

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -18,3 +18,24 @@ public class RewardsConverter {
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
+
+class RewardValue {
+    private double cashValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int miles) {
+        this.cashValue = miles * MILES_TO_CASH_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return (int) (cashValue / MILES_TO_CASH_RATE);
+    }
+}
