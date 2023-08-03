f9f2840f1f35faec54e7b0ee9fe92767000cb5fa
Jayaharsha Panuganti <panuganti.jayaharsha@gmail.com>
2023-08-03 16:06:52 -0700

    Providing the RewardValue class.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,26 +1,34 @@
 /*
-This class is functions as a converter for miles and cash. I created two variables: cashValue and miles, and made them
-private to not allow the users of RewardsConverter class to change the values. I also created two get methods that convert
-cash to miles and miles to cash and providing the users the conversion.
- */
+This class, RewardValue, functions as a two-way converter between miles and cash values. It allows users to create objects
+with either a cash value or a miles value, but once set, the cash value remains constant and cannot be modified directly
+by external users. The class provides methods to obtain the cash value and the corresponding miles value based on a fixed
+conversion rate of 0.0035 miles per unit of cash. The private conversion methods handle the actual conversion calculations,
+ensuring accurate and consistent conversions between miles and cash.
+*/
 public class RewardValue {
+    private final double cashValue;
 
-    private double cashValue;
-    private float milesValue;
-    public RewardValue(double cashValue){
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
-    public RewardValue(float milesValue){
-        this.milesValue = milesValue;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public double getCashValue(){
-        return milesValue * 0.0035;
+    private int convertToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
     }
 
-    public float getMilesValue(){
-        return (float) (cashValue / 0.0035);
+    private double convertToCash(int miles) {
+        return miles * 0.0035;
     }
 
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
