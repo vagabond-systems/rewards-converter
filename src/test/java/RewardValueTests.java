23dfd186c9c12e7f6649dd6577673fd41b2cd9c9
Aditya-Reniwal <141135104+Aditya-Reniwal@users.noreply.github.com>
2023-08-03 22:21:09 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -20,11 +19,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMiles = (int) (cashValue / 0.0035); // Conversion rate: 0.0035
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035; // Conversion rate: 0.0035
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.01); // Adding a delta for double comparison
     }
 }
