a142ce01d04b949c1477494dfe6d11a5309e06ef
Jayaharsha <panuganti.jayaharsha@gmail.com>
2023-08-03 14:43:12 -0700

    Providing the RewardValue class.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -6,17 +6,17 @@ cash to miles and miles to cash and providing the users the conversion.
 public class RewardValue {
 
     private double cashValue;
-    private float miles;
+    private float milesValue;
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
     }
 
-    public RewardValue(float miles){
-        this.miles = miles;
+    public RewardValue(float milesValue){
+        this.milesValue = milesValue;
     }
 
     public double getCashValue(){
-        return miles * 0.0035;
+        return milesValue * 0.0035;
     }
 
     public float getMilesValue(){
