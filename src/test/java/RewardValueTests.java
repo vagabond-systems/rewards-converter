645d40c67bc2dd91ccdde23d4cf91bbdb9fff214
unknown <kishori.bhojane9@gmail.com>
2023-08-05 00:16:30 -0500

    Second Commit

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -19,12 +19,19 @@ public class RewardValueTests {
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    void convert_from_cash_to_miles() {//assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.001);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        //assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue); // Marking the constructor explicitly as miles
+        double expectedCash = milesValue * 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.001);
     }
 }
