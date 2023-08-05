de58612409edc15cc6a208a873e43b17cdb69b95
Eddie Wynman <edward.wynman@gmail.com>
2023-08-04 21:21:14 -0400

    Task 1

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -13,8 +13,45 @@ public class RewardsConverter {
             System.out.println("Could not parse input value as a double, exiting");
             return;
         }
-        System.out.println("converting $" + input_value + " to miles");
+        System.out.println("Converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+    }
+}
+
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValueToMiles(cashValue);
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesToCashValue(milesValue);
+    }
+
+    // Get the cash value of the RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get how many miles the RewardValue is worth
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    // Convert cash value to miles
+    private double cashValueToMiles(double cashValue) {
+        return cashValue / 0.0035;
+    }
+
+    // Convert miles to cash value
+    private double milesToCashValue(int milesValue) {
+        return milesValue * 0.0035;
     }
 }
