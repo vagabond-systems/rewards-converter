70c18db9404d98d2f1f91f2dc32217fa9c9f222a
George Spanodimos <georgespanodimos1@gmail.com>
2023-08-11 00:24:54 +0300

    Changed the last two test functions to check if the conversion is being done during instantiation of the reward value object

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int TrueMiles = (int)(cashValue/0.0035);
+        assertEquals(TrueMiles,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double TrueCash = milesValue * 0.0035;
+        assertEquals(TrueCash, rewardValue.getCashValue());
     }
-}
+    }
+
