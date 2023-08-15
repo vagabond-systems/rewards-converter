9d728fc67080d654c5854e4f80e4cedb79ce01e6
Aditya <aditya.datla98@gmail.com>
2023-08-15 01:16:20 -0500

    Fixed RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,27 @@
 public class RewardValue {
   public double cash;
-  public double miles;
 
   public RewardValue(double cash) {
     this.cash = cash;
   }
 
-  public RewardValue(double cash, double miles){
-    this.cash = cash;
-    this.miles = miles;
+  public RewardValue(int miles){
+    this.cash = convertToCash(miles);
   }
 
   public double getCashValue(){
-    cash = miles * .0035;
     return cash;
   }
 
-  public String getMilesValue() {
-    miles = cash / .0035;
-    String milesStr = "" + miles;
-    return milesStr;
+  public int getMilesValue() {
+    return convertToMiles(cash);
+  }
+
+  private static double convertToCash(int miles){
+    return miles *.035;
+  }
+
+  private static int convertToMiles(double cash){
+    return (int) (cash / .035);
   }
 }
