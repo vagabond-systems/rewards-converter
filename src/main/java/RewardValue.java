2cce78d587fb31b572d6e2ddf8b88124db85bde3
caryn <caryn.suli@gmail.com>
2023-08-22 15:35:36 +0100

    learning based on model answer

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,30 @@
 public class RewardValue {
     private double cashValue;
-    private int milesValue;
+    public static final double CONVERSION_RATE = 0.0035;
 
-    RewardValue(double cash){
-        cashValue = cash;
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
     }
 
-    RewardValue(int miles){
-        milesValue = miles;
+    public RewardValue(int milesValue){
+        this.cashValue = convertToCash(milesValue);
     }
 
+    private static int convertToMiles(double cashValue){
+        return (int) (cashValue/CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue){
+        return milesValue * CONVERSION_RATE;
+    }
     public double getCashValue(){
         //returns the cash value of the RewardValue
-        return milesValue / 0.0035;
+        return cashValue;
     }
 
     public int getMilesValue(){
         //returns how many miles the RewardValue is worth
-        return (int) (cashValue * 0.0035);
+        return convertToMiles(cashValue);
     }
 
 }
