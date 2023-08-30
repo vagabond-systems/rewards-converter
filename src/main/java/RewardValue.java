3c8ae187570cfdef7df7c7da93e645a372fd8cbe
Saumyadeep Ray <saumyadeepray0403@gmail.com>
2023-08-30 16:29:38 +0530

    reward-value-test-1

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,21 @@
 public class RewardValue {
-    
+
     public double cashValue;
     public double milesValue;
+    public static final double conversionRate = 0.0035;
 
     // Constructor that accepts Cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
         // Convert Cash to Miles
-        this.milesValue = cashValue / 0.0035;
+        this.milesValue = cashValue / conversionRate;
     }
 
     // Constructor that accepts Miles value
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
         // Convert Miles to Cash
-        this.cashValue = milesValue * 0.0035;
+        this.cashValue = milesValue * conversionRate;
     }
 
     // Get the Cash value of the Reward value
@@ -24,6 +25,6 @@ public class RewardValue {
 
     // Get how many miles the reward value is worth
     public int getMilesValue() {
-        return (int)milesValue;
+        return (int) milesValue;
     }
 }
