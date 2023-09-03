124c9e0a39c8a3e72b8ed07272bc8fa31f8ddc1f
safeer <safeeralam997@gmail.com>
2023-09-03 17:25:59 +0530

    test-cases-added

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -13,18 +13,24 @@ public class RewardValueTests {
 
     @Test
     void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        double milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue, true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMilesValue = cashValue / 0.0035;
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue, true);
+        double expectedCashValue = milesValue * 0.0035;
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
