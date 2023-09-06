cfb76616748b666838484dfd947d560fa1ac6d88
Rishabh Aggarwal <aggarwal.rishabh41@gmail.com>
2023-09-06 22:04:35 +0100

    modified RewardValue

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,5 +1,5 @@
 public class RewardValue {
-    
+
     private double cashValue;
     private double milesValue;
     private final double MILES_CONVERSION_RATE = 0.0035;
@@ -7,13 +7,13 @@ public class RewardValue {
     // Constructor accepting cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue * MILES_CONVERSION_RATE;
+        this.milesValue = cashValue / MILES_CONVERSION_RATE;
     }
 
     // Constructor accepting miles value
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        this.cashValue = milesValue / MILES_CONVERSION_RATE;
+        this.cashValue = milesValue * MILES_CONVERSION_RATE;
     }
 
     // Method to get the cash value
