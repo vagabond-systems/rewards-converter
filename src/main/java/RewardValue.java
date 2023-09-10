2b3e5a180ec42bf10de049ee73ac0411a047a07b
Chirag <69969661+C-h-i-rag@users.noreply.github.com>
2023-09-10 14:25:08 +0530

    Refractored Test Cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,18 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-    private int milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035); // Convert cash to miles
     }
+
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035; // Convert miles to cash
+        this.cashValue = convertToCash(milesValue);
     }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
     public double getCashValue() {
         return cashValue;
     }
+
     public int getMilesValue() {
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
 }
