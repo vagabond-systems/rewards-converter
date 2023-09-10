b463d1c47a0e1ed63769c9f1c46fc075ff5241bf
Jide Oyelayo <JideOyelayo1@Gmail.com>
2023-09-10 16:09:42 +0100

    Added unit tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+    final double converstionfactor = 0.0035;
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -21,7 +21,7 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
-        int milesValue = 28571;
+        int milesValue = (int)(cashValue/converstionfactor);
         var rewardValue = new RewardValue(cashValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
@@ -29,8 +29,9 @@ public class RewardValueTests {
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 100;
-        double cashValue = 0.35;
+
         var rewardValue = new RewardValue(milesValue);
+        double cashValue = milesValue*converstionfactor;
 
         double tolerance = 0.009; // to check if number is correct nearest pence
         assertEquals(cashValue, rewardValue.getCashValue(),tolerance);
