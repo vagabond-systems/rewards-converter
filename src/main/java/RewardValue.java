326040c564b916048a570987eb00f9fc2dd7e3bf
Sharkywrecks <sharkywrecks@gmail.com>
2023-09-12 10:56:50 +0100

    Task 1 - Revised

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,15 +1,28 @@
 public class RewardValue {
-    private final double cash;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE=0.0035;
 
-    public RewardValue(double cash) {
-        this.cash = cash;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int)(cashValue/MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue*MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
-        return cash;
+        return cashValue;
     }
 
     public double getMilesValue() {
-        return cash/0.0035;
+        return convertToMiles(cashValue);
     }
 }
