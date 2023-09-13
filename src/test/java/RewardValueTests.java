95077a421d101ecd683f87b0a4ac3e6794b1f703
janakiram11072002 <janakiram11072002@gmail.com>
2023-09-13 21:09:24 +0530

    Implemented RewardValue TestCAse.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMileValue = (int) (cashValue/RewardValue.MILES_TO_CASH_CONVERSION_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMileValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int mileValue = 10000;
+        double expectedCashValue = mileValue*RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+        var rewardValue = new RewardValue(mileValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
