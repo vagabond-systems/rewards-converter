e80623da2e7f6f27a57bda55d6582587a09a7ab7
Pritesh2k <priteshparekh237@gmail.com>
2023-09-14 14:48:28 +0100

    Added in the RewardsValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(int miles) {
+        this.milesValue = miles;
+        this.cashValue = miles * 0.0035;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
