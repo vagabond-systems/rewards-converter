0519b820718df8050236abd39e6dd11e9968ce3d
FOPA KUETE DUCLAIR <fopaduclair2000@dimsoft.eu>
2023-09-16 15:42:13 +0100

    Task 1: Modify an existing system

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor that accepts miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * 0.0035;
+        } else {
+            throw new IllegalArgumentException("Invalid constructor usage. Use the constructor with cash value.");
+        }
+    }
+
+    // Get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
