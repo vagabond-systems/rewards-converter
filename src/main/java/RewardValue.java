af11e6994c773bc6419e07b2d993df0ac56d4fa7
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2023-09-16 17:09:10 +0800

    Change some method to privates, and make all test passed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,10 +1,10 @@
 public class RewardValue {
     double cash;
-    static final double M_TO_CASH_RATE = 0.0035;
-    double MilesToCash(int miles){
+    private static final double M_TO_CASH_RATE = 0.0035;
+    private double MilesToCash(int miles){
         return miles * M_TO_CASH_RATE;
     }
-    int CashToMiles(double cash){
+    private int CashToMiles(double cash){
         return (int)(cash / M_TO_CASH_RATE);
     }
     public RewardValue(double cash){
