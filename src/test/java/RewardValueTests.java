6499fb33098e6d3acb36d716f729dfcddadb6eb2
Viraj Paradkar <virajsparadkar@gmail.com>
2023-09-21 09:37:57 +0530

    Task II Complete

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    private static final double CONVERSION_RATE = 0.0035;
+
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +22,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / CONVERSION_RATE;
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * CONVERSION_RATE;
+        assertEquals(expectedCash, rewardValue.getCashValue());
     }
 }
