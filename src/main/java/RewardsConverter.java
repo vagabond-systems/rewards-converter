9750c9e3731c2b6d77b7c66cefdde210d9733006
Viraj Paradkar <virajsparadkar@gmail.com>
2023-09-21 09:31:46 +0530

    Task Complete

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / CONVERSION_RATE;
+    }
+
+    public RewardValue(long milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
