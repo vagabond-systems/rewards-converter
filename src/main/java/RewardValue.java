4a824b427a0216f66037b46f6589cae7a8f0a754
unknown <shavez192@gmail.com>
2023-09-23 07:10:21 +0530

    added testsuitecase

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,16 +1,28 @@
-//import java.util.Scanner;
-
-
 public class RewardValue {
-    
-  public double milesValue;
-   
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-	public RewardValue(double cashValue) {
-		this.milesValue = 2*cashValue;	
-	}
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
 
-	public double getMilesValue() {
-	 return milesValue; 		
-	}
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
