859b812e7dff1c9e2fa122c64ddfe9391c00c631
Omkar Ubale <oubale@gmail.com>
2023-10-06 22:13:03 -0400

    SETUP: added class skeleton for RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,18 @@
+public class RewardValue {
+
+  public RewardValue(int miles) {
+
+  }
+
+  public RewardValue(double cash) {
+
+  }
+
+  public double getCashValue() {
+    return 0;
+  }
+
+  public int getMilesValue() {
+    return 0;
+  }
+}
