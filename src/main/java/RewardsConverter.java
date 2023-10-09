06e6affa032c2926cbd173ad3b2ea31857cb459a
Nn170 <navneetmaan198@gmail.com>
2023-10-09 16:55:43 -0700

    added class rewardValue

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 
+class RewardValue{
+    private double cashValue;
+    private int milesValue;
+
+    RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = (int)(cashValue/0.0035);
+    }
+
+    RewardValue(int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public int getMilesValue(){
+        return milesValue;
+    }
+
+}
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
