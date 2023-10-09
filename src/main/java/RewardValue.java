92773111fd95b9e8ac9a517c6a46690f97de769c
Vishal <vishalvarun27@gmail.com>
2023-10-10 01:56:26 +0530

    Added RewardValue class to read and convert cashvalue to miles.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
 public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+    private final double miles_to_cash_rate = 0.0035;
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue =(int)(cashValue/miles_to_cash_rate);
+    }
+    public RewardValue(int milesValue){
+        this.cashValue=milesValue * miles_to_cash_rate;
+        this.milesValue=milesValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+
 }
