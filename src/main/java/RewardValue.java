6c5f2a6cace2fe5680f5b7a0e631f1faf0a7fa73
pranavhajare <hajare.pranav@gmail.com>
2023-10-11 13:28:35 +0530

    test cases completed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,32 +1,22 @@
 public class RewardValue {
+    private double cash;
+    private int miles;
 
-    private double cashValue;
-    private double milesValue;
-
-    // Constructor that accepts a cash value
-    public RewardValue(double cashValue) {
-        this.cashValue = cashValue;
-        this.milesValue = cashValue * 0.0035;
+    public RewardValue(double cash){
+        this.cash = cash;
+        this.miles = (int) (cash / 0.0035);
     }
 
-    // Constructor that accepts a value in miles
-    public RewardValue(double milesValue, boolean isMiles) {
-        if (isMiles) {
-            this.milesValue = milesValue;
-            this.cashValue = milesValue / 0.0035;
-        } else {
-            this.cashValue = milesValue * 0.0035;
-            this.milesValue = milesValue;
-        }
+    public RewardValue(int miles) {
+        this.miles = miles;
+        this.cash = miles * 0.0035;
     }
 
-    // Get the cash value of the RewardValue
-    public double getCashValue() {
-        return cashValue;
+    public double getCashValue(){
+        return cash;
     }
 
-    // Get how many miles the RewardValue is worth
-    public double getMilesValue() {
-        return milesValue;
+    public double getMilesValue(){
+        return miles;
     }
 }
