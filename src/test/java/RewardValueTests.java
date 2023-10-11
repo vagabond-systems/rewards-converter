201b2c592aae662cc4e6c53ede0f389d90463f69
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2023-10-11 13:38:19 +0530

    Update RewardValueTests.java test passed

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,33 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class RewardValueTests {
-
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        assertEquals(cashValue, rewardValue.getCashValue(), 0.001); // Using a delta for double comparison
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        var rewardValue = new RewardValue(milesValue, true);
+        assertEquals(milesValue, rewardValue.getMilesValue(), 0.001); // Using a delta for double comparison
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMilesValue = cashValue / 0.0035;
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue(), 0.001); // Using a delta for double comparison
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue, true);
+        double expectedCashValue = milesValue * 0.0035;
+        assertEquals(expectedCashValue, rewardValue.getCashValue(), 0.001); // Using a delta for double comparison
     }
 }
