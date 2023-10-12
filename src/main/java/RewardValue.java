67eb04bdc3bed99ec5a5b471f6cea989848af768
Student 27 <student27@Codes-MacBook-Air-9.local>
2023-10-11 20:40:30 -0400

    finalized 2 additional unit test

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    public RewardValue(double milesValue) { // must convert from miles to cash
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -19,12 +19,20 @@ public class RewardValueTests {
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    void convert_from_cash_to_miles(){
+        RewardsConverter rewardsConverter = new RewardsConverter();
+        double cashValue = 0;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue / 0.0035, rewardValue.getMilesValue());
+
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        RewardsConverter rewardsConverter = new RewardsConverter();
+        int milesValue = 0;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue * 0.0035, rewardValue.getCashValue());
+
     }
 }
