1284813ceb8d0f44a89ed705931f8b0d2517642c
Nupur-s <nupursaboo01@gmail.com>
2023-10-13 21:41:31 +0530

    Added necessary file

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    double cashValue;
+    long miles;
+     public RewardValue(double cashValue){
+         this.cashValue = cashValue;
+     }
+    public RewardValue(long miles){
+        this.miles = miles;
+    }
+
+    public double getCashValue() {
+        return miles/0.0035;
+    }
+    public double getMilesValue() {
+         return cashValue*0.0035;
+    }
 }
