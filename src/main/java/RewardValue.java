0b43f3ee46155ff77929768068f645ced69fe68e
Kushagra Singh <73294651+KushagraSingh02@users.noreply.github.com>
2023-10-14 11:34:58 +0530

    final update

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,28 @@
 public class RewardValue {
+    private final double cashValue;
 
-    private double cashval;
-    private int milesval;
 
-    public RewardValue( double cash){
-
-        cashval = cash;
-        milesval = (int) (cashval/0.0035);
-    }
-    public RewardValue(int miles){
-        milesval = miles;
-        cashval = milesval * 0.0035;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    public double getCashValue(){
-
-        return cashval;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public double getMilesValue(){
-        return milesval;
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
