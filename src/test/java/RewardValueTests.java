d5b482049b57d5f91e7c7495a3c7ef0022216efb
Soumya <soumyasrivastava0131@gmail.com>
2023-10-15 21:47:50 +0530

    Adding RewardValue class

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int equivalentMilesvalue = (int)(cashValue / 0.0035);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(equivalentMilesvalue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double equivalentCashvalue = milesValue * 0.0035;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(equivalentCashvalue, rewardValue.getCashValue());
     }
 }
