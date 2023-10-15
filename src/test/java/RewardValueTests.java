4f81f8b8ada494354071a32af09015c5bbf0d264
sarth <sarth.patel1411@gmail.com>
2023-10-15 16:50:42 +0530

    commit

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,17 +20,11 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        int expectedMilesValue = (int) (cashValue / 0.0035);
-        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
+        assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        double expectedCashValue = milesValue * 0.0035;
-        assertEquals(expectedCashValue, rewardValue.getCashValue());
+        assert false;
     }
 }
