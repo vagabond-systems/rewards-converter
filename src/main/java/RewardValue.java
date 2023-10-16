f98f905285d0790105ef10aa5d7423dcf72ff630
DevSasidhar <sasidharon1142002@gmail.com>
2023-10-16 19:12:52 +0530

    RewardValue class added

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,30 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-    private double milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashToMiles(cashValue);
     }
 
-    public RewardValue(double milesValue, boolean isMiles) {
-        this.milesValue = milesValue;
-        this.cashValue = milesToCash(milesValue);
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        return milesValue;
-    }
-
-    private double cashToMiles(double cashValue) {
-        return cashValue / 0.0035;
-    }
-
-    private double milesToCash(double milesValue) {
-        return milesValue * 0.0035;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
