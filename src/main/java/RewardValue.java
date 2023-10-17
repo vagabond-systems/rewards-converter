c8c3a019441b8f39495c5de551108617532b7951
ravi vaghela <ravivaghela26@gmail.com>
2023-10-16 21:30:18 -0400

    Added RewardValue Class with getCashValue and getMilesValue methods

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+public class RewardValue {
+    double cash;
+    float miles;
+
+    final double MILES_TO_CASH_RATIO= 0.0035;
+
+    public RewardValue(double cash){
+        this.cash = cash;
+    }
+    public RewardValue(float miles){
+        this.miles = miles;
+    }
+
+    public double getCashValue(){
+        return this.cash;
+    }
+    public double getMilesValue(){
+        return this.cash*MILES_TO_CASH_RATIO;
+    }
+
+}
