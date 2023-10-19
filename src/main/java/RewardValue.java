85d6245060f73cf40e44559ddf61a6374ed3413a
Shubham Nayak <nayakshubham2002@gmail.com>
2023-10-19 23:02:33 +0530

    made some changes in the unit Tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Getter method for cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Getter method for miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
