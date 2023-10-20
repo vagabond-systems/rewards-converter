ac80d4c36f61b0a8a9d646c4ae7688b0bcd7aaa3
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-10-21 04:38:31 +0530

    testcases completed

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    double MilesToCashRate = 0.0035;
+
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +22,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMilesValue = (int)cashValue/MilesToCashRate;
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue*MilesToCashRate;
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
