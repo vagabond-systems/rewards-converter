8f12399731b8d691bb1f232a81e1e2e2818adfe5
ritika-1007 <96466380+ritika-1007@users.noreply.github.com>
2023-10-22 13:05:21 +0530

    task1 done

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,26 @@
 public class RewardValue {
-    double cashValue,milesValue;
+    double cashValue;
+    public static final double milesToCash=0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+    }
+    public RewardValue(int milesValue){
+        this.cashValue=convertToCash(milesValue);
+    }
+    public double convertToCash(int milesValue){
+        return milesValue*milesToCash;
+    }
+    public int convertToMiles(double cashValue){
+        return (int) (cashValue/milesToCash);
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        milesValue=cashValue*0.0035;
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 
 }
