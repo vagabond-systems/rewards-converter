ce0538c71fc397247cf6893b591d064fdacfc780
KomalJain <komaljain9603@gmail.com>
2023-10-23 10:02:38 +0530

    Changes

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,28 @@
 public class RewardValue {
     private final double cashValue;
-    private final int milesValue;
-    public RewardValue(double cashValue){
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int)(cashValue / 0.0035);
     }
 
-    public RewardValue(int milesValue){
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public double getCashValue(){
-        return  cashValue;
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
     }
 
     public int getMilesValue() {
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
-
 }
