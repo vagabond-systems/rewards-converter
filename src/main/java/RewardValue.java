9d67372c5fbdea20743c0fe1bfde5b5f4e560cd3
Kunal Gautam <mailtokunal007@gmail.com>
2023-10-28 22:09:12 +0530

    Committed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,36 +1,27 @@
-import java.util.Scanner;
 public class RewardValue {
-        private double cashValue;
-        private double milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-        // Constructor that accepts cash value
-        public RewardValue(double cashValue) {
-            this.cashValue = cashValue;
-            this.milesValue = cashValue / 0.0035;
-        }
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
 
-        // Constructor that accepts miles value
-        public RewardValue(double milesValue, boolean isMiles) {
-            if (isMiles) {
-                this.milesValue = milesValue;
-                this.cashValue = milesValue * 0.0035;
-            } else {
-                // Handle the case where the constructor is called with miles but isMiles is false
-                this.milesValue = 0;
-                this.cashValue = 0;
-            }
-        }
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
 
-        // Method to get the cash value
-        public double getCashValue() {
-            return cashValue;
-        }
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
 
-        // Method to get the miles value
-        public double getMilesValue() {
-            return milesValue;
-        }
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
 
+    public double getCashValue() {
+        return cashValue;
+    }
 
-
-}
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }}
