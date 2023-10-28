cd9bea1c80be5b425b0bd9a206ccc98aaebd4981
fathizjm <fsamreen14@gmail.com>
2023-10-28 17:12:39 +0530

    Task 2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,25 @@
-public class RewardValue{
-	private double cash;
-	private int miles;
 
-	public RewardValue(double cash) {
+
+public class RewardValue {
+    private double cash;
+    private int miles;
+
+    // Constructor for cash value
+    public RewardValue(double cash) {
         this.cash = cash;
         this.miles = (int) (cash / 0.0035); // Conversion rate: 1 mile = 0.0035 cash
     }
 
-	public RewardValue(int miles) {
+    // Constructor for miles value
+    public RewardValue(int miles) {
         this.miles = miles;
-        this.cash = cash * 0.0035; // Conversion rate: 1 mile = 0.0035 cash
+        this.cash = miles * 0.0035; // Conversion rate: 1 mile = 0.0035 cash
     }
 
     public double getCashValue() {
         return cash;
     }
 
-    // Method to get miles value
     public int getMilesValue() {
         return miles;
     }
