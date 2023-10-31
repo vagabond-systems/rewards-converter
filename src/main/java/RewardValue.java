b894ecb7a94afba8a08f291ad86ef23aeb7dc83e
Bhukya Saritha <rathodsaritha699@gmail.com>
2023-10-31 22:51:59 +0530

    new changes

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,24 @@
 public class RewardValue {
     private final double cashValue;
-    private final double milesValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035;
     }
 
     // Constructor that accepts a value in miles
-    public RewardValue(double milesValue, boolean isMiles) {
-        if (isMiles) {
-            this.milesValue = milesValue;
-            this.cashValue = milesValue * 0.0035;
-        } else {
-            this.milesValue = 0;
-            this.cashValue = 0;
-        }
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
 
     // Method to get the cash value of the RewardValue
     public double getCashValue() {
@@ -26,6 +27,6 @@ public class RewardValue {
 
     // Method to get the miles value of the RewardValue
     public double getMilesValue() {
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
 }
