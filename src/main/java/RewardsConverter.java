a21076737f20bd1f46c69693cb84b79528335d97
GAURAV <gauravlamba888@gmail.com>
2023-11-02 15:06:57 +0530

    Created Reward-Value

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
+class RewardValue {
+    private double val;
+    public RewardValue(double val){
+        this.val = val;
+    }
+    public double getMilesValue(){
+        double miles = 0;
+            miles = (val*10000)/35;
+        return miles;
+    }
+    public double getCashValue(){
+        double cash = 0;
+            cash = 0.0035*val;
+        return cash;
+    }
+}
+
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
