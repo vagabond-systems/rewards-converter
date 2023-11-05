b97adfab9cea65b0b0325e851c9a0bd08672ba78
AHK018 <ameykhamkarcsr@gmail.com>
2023-11-05 15:57:28 +0530

    second Commit

--- src/main/java/RevardValue/RewardValue.java
+++ src/main/java/RevardValue/RewardValue.java
@@ -2,15 +2,13 @@ package RevardValue;
 
 public class RewardValue {
     private double cashValue;
-    private double conversionRate;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.conversionRate = 0.1;
     }
 
     public double getMilesValue() {
-        return cashValue * conversionRate;
+        return cashValue * 0.0035;
     }
 
     public double getCashValue() {
