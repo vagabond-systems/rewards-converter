cb6df12caf269b9d4fe49a759884307e8ca88e29
Bhavya Singhal <singhalbhavya10@gmail.com>
2023-11-07 17:33:18 +0530

    adding required functions

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,28 @@
 public class RewardValue {
-    private double cashValue;
-    private double milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    public RewardValue(double cashValue){
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035;
-    }
-    
-    public RewardValue(double milesValue, boolean isMiles){
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035;
     }
 
-    public double getCashValue(){
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue(){
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
-
 }
