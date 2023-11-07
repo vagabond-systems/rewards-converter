cd2c33d4f7dc10b148b9ec60e460003120667687
Leo <deutschleonardo@gmail.com>
2023-11-07 13:36:33 -0600

    Restarting test example.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,17 +20,11 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
+
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10001;
-        double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(expectedCashValue, rewardValue.getCashValue());
+
     }
 }
