908219d8e15f6579f38d07c284ae31eda6287789
Jiacheng-Yuan <cathyyuan1111@gmail.com>
2023-11-07 17:17:13 +1100

    update rewardValue and tests for convert miles/cash

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Method to get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get miles value
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
