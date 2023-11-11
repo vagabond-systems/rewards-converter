8d6f7b4f71bd2f52bec677c7522333548dff0760
David Nguyen <da791195@gmail.com>
2023-11-11 14:50:00 -0600

    Test suite has been updated again!

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -21,16 +21,16 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
         var rewardValue = new RewardValue(cashValue);
-        rewardValue.getMilesValue();
-        assert false;
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
     	int milesValue = 10000;
-    	var rewardValue = new RewardValue(milesValue);
-    	rewardValue.getCashValue();
-        assert false;
+    	double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
