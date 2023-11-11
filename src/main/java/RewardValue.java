b176af540d1f51a2343c31d3d929995faa776c03
David Nguyen <da791195@gmail.com>
2023-11-11 13:55:00 -0600

    Corrections have been made!

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,28 @@
 public class RewardValue {
-	private double cashValue;
-	private int milesValue;
+	private final double cashValue;
+	public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 	
 	public RewardValue(double cashValue) {
-		this.cashValue = cashValue;
-	}
-	
-	public RewardValue(int milesValue) {
-		this.milesValue = milesValue;
-	}
-	
-	public double getCashValue() {
-		return milesValue * 0.0035;
-	}
-	
-	public double getMilesValue() { 
-		return (double)(cashValue) / 0.0035;
-	}
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
