d694497b5d6d7fe54d307c66b06d741905094d9c
Abrar Hussain <117599985+sahussain887@users.noreply.github.com>
2023-11-15 15:39:19 -0600

    RewardValueTests updated to pass test cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,40 +1,38 @@
 public class RewardValue {
-//    variables:
-    private double cashValue;
-    private double milesValue;
 
-//    constructor accepting cash val
-    public RewardValue(double cashValue){
+    // Cash value of the reward
+    private final double cashValue;
+
+    // Conversion rate from miles to cash
+    public static final double conversionRate = 0.0035;
+
+    // Constructor taking cash value as input
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = convertToMiles(cashValue);
     }
 
-//    constructor accepting val in miles
-    public RewardValue(double milesValue, boolean isMiles){
-        if(isMiles){
-            this.milesValue = milesValue;
-            this.cashValue = convertToCash(milesValue);
-        } else {
-            System.out.println("Constructor error");
-        }
+    // Constructor taking miles value as input
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-//    Getter
-    public double getCashValue(){
+    // Private method to convert cash value to miles
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / conversionRate);
+    }
+
+    // Private method to convert miles value to cash
+    private static double convertToCash(int milesValue) {
+        return milesValue * conversionRate;
+    }
+
+    // Getter method for cash value
+    public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue(){
-        return milesValue;
-    }
-
-//    convert miles to cash
-    private double convertToCash(double milesValue){
-        return milesValue * 0.0035;
-    }
-
-//    convert cash to miles
-    private  double convertToMiles(double cashValue){
-        return cashValue / 0.0035;
+    // Getter method for miles value, converting cash to miles
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
