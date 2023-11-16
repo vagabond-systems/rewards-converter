ca69a84a46284abdb0e3e1d38afe59f0a45b1d7a
Thinh Vo <thinhmvo101@gmail.com>
2023-11-16 17:41:14 -0500

    set up value and conversion

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,29 @@
 public class RewardValue {
+    private int mileValue;
     double cashValue;
+    final double MILE_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
+
+    //Java require overloaded constructor to have different types of sequences of parameters
+    //So I'm assuming there are no partial miles(e.g. 0.5 miles)
 
     public RewardValue(int mileValue) {
         this.mileValue = mileValue;
     }
 
     public double getCashValue() {
-        return cashValue;
+        //convert cash value to miles
+        return this.cashValue;
     }
 
     public int getMilesValue() {
-        return mileValue;
+        //convert miles value to cash
+        //At this stage of the project, I'm assuming that get MileValues is the only
+        //method that do the conversion. In the future, I expected to have a method that
+        //do the calculation and return the value separately.
+        return (int) (cashValue*MILE_TO_CASH_CONVERSION_RATE);
     }
-
-    int mileValue;
-
 }
