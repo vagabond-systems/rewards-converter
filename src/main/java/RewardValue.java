25003d899d59e76cc05dba4b2268644f72fb10de
Balachander <72781431+Balachander283@users.noreply.github.com>
2023-11-18 02:46:14 +0530

    Changes Added

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
