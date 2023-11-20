5bf706dd8003d59c1de0acdddcdb83e67803d962
felix <felixvjr0@gmail.com>
2023-11-20 12:47:31 -0500

    adding cash & miles test cases converting

--- src/main/java/model/RewardValue.java
+++ src/main/java/model/RewardValue.java
@@ -2,6 +2,7 @@ package model;
 
 public class RewardValue {
     private final double cashValue;
+    // conversion rate from dollars to miles
     public static final double MILES_CONVERT = 0.0035;
 
     public RewardValue(double cashValue) {
