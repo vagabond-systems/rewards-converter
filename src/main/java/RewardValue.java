e5ee1de078b520119a74385d47407f0ca996765d
KuromeMochi <pinguoboy@gmail.com>
2023-11-22 23:34:05 +0000

    Added unit tests for conversions, utilise methods with calculations

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,7 +1,7 @@
 public class RewardValue {
     private double cashValue;
     private double milesValue;
-    double milesCashRate = 0.0035;
+    public static double milesCashRate = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
@@ -15,7 +15,20 @@ public class RewardValue {
         return this.cashValue;
     }
 
-    public double getMilesValue(){
-        return this.cashValue * milesCashRate;
+    public int getMilesValue(){
+        if (this.milesValue != 0) { // if set via initialization
+            return (int)this.milesValue;
+        }
+        else {
+            return (int)(this.cashValue / milesCashRate);
+        }
+    }
+
+    public int cashToMiles(double cash){
+        return (int)(cash / milesCashRate);
+    }
+
+    public double milesToCash(double miles){
+        return miles * milesCashRate;
     }
 }
