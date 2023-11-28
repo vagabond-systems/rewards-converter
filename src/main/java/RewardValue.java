2a036d132c92e64db7d5ee631aca95db4c829a3f
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-27 21:32:04 -0600

    added new class for ticket and added comments for direction

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,10 @@
+public class RewardValue {
+
+}
+
+
+
+//    RewardValue must have two constructors: one that accepts a cash value and one that accepts a value in miles.
+//    RewardValue must have a getCashValue() method, which returns the cash value of the RewardValue.
+//    RewardValue must have a getMilesValue() method, which returns how many miles the RewardValue is worth.
+//    RewardValue must convert from miles to cash at a rate of 0.0035.
