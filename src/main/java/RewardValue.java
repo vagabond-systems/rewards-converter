a4dc8d7b33c32ef8d593da0348c9250f5fdf0b57
Clifford Gardner <cliffordgardner9@gmail.com>
2023-11-29 11:33:46 -0600

    Finished Task 2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,6 +2,14 @@ public class RewardValue {
     private double cash;
     private float miles;
 
+    public double getCash() {
+        return cash;
+    }
+
+    public float getMiles() {
+        return miles;
+    }
+
     RewardValue(double cash){
         this.cash = cash;
     }
@@ -9,7 +17,7 @@ public class RewardValue {
         this.miles = miles;
     }
     public double getCashValue(){
-        return cash;
+        return miles * .0035;
     }
     public float getMilesValue(){
         return (float) (cash / .0035);
