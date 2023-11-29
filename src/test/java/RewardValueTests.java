d046174183393152e6757ed6ffdf452a1942a810
SteampunkWhales <100243497+SteampunkWhales@users.noreply.github.com>
2023-11-29 15:52:00 -0500

    created convert_from_cash_to_miles and convert_from_miles_to_cash tests.

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,21 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        int milesExpected = 10000;
+        double cashValue = 35.00;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        int milesActual = rewardValue.getMilesValue();
+        assertEquals(milesExpected, milesActual);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        double cashExpected = 3.50;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        double cashActual = rewardValue.getCashValue();
+        assertEquals(cashExpected, cashActual);
+
+
     }
 }
