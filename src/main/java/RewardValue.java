f34136a052f99ea74c93b7d125033386b9be3c51
Forrest Yankey <forrestyankey@gmail.com>
2023-11-29 21:32:24 -0500

    edited code to pass test, completed tests, changed RewardsConverter.java to use new method

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,8 +1,8 @@
 import java.math.BigDecimal;
 
 public class RewardValue {
-    private double cash;
-    private int miles;
+    private double cash = 0.0;
+    private int miles = 0;
 
     public RewardValue(double cash) {
         this.cash = cash;
@@ -13,32 +13,18 @@ public class RewardValue {
     }
 
     public double getCashValue() {
-        double cashValue;
-        cashValue = this.miles / 0.0035;
-        return cashValue;
+        return this.cash;
     }
 
     public double getMilesValue() {
-        int milesValue;
-        milesValue = (int)(this.cash * 0.0035);
-        return milesValue;
-    }
-
-
-    //GETTERS & SETTERS
-    public double getCash() {
-        return cash;
-    }
-
-    public void setCash(double cash) {
-        this.cash = cash;
-    }
-
-    public double getMiles() {
         return this.miles;
     }
 
-    public void setMiles(int miles) {
-        this.miles = miles;
+    public double convertMilesToCash() {
+        return this.miles / 0.0035;
+    }
+
+    public int convertCashToMiles() {
+        return (int)(this.cash * 0.0035);
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 1000.00;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(3, rewardValue.convertCashToMiles());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(285714.2857142857, rewardValue.convertMilesToCash());
     }
 }
