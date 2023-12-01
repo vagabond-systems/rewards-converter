c8a007e4d0b1d32a3a3b25571a205b01020dfdef
Taalaikul <taalaikul.mj@gmail.com>
2023-11-30 21:05:34 -0500

    added Rewards Value Class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,29 @@
 public class RewardValue {
 
-    private double cashValue;
-    private double milesValue;
-    public static final double CONVERSION_RATE = 0.0035;
-
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue * CONVERSION_RATE;
     }
 
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
