cc75a3e3e92bdfb3faf7c2e3c7cbd898b29fb57b
unclebabykern <coren.frankel@gmail.com>
2023-11-30 18:57:57 -0700

    correct arithmetic in RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,10 +2,12 @@ public class RewardValue {
     private double cash;
     private int miles;
     public RewardValue(double cash) {
+        this.miles =  (int) (cash / 0.0035);
         this.cash = cash;
     }
     public RewardValue(int miles) {
         this.miles = miles;
+        this.cash = this.miles * 0.0035;
     }
 
     public double getCashValue() {
