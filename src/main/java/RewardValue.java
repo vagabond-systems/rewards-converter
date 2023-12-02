b566f90c715f13e6594bb17e84e3d537daeb8933
Warren Chan <warren.chan0113@gmail.com>
2023-12-02 15:52:17 -0500

    made improvements to RewardValue file

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,15 +1,26 @@
 public class RewardValue {
-    double cash;
-    int miles;
+    private final double cash;
+    public static final double miles_to_cash_conversion_rate = 0.0035;
     public RewardValue(double cash) {
         this.cash = cash;
     }
+    public RewardValue(int miles) {
+        this.cash = convertToCash(miles);
+    }
+
+    private static int convertToMiles(double cash) {
+        return (int) (cash / miles_to_cash_conversion_rate);
+    }
+
+    public static double convertToCash(int miles) {
+        return miles * miles_to_cash_conversion_rate;
+    }
 
     public double getCashValue() {
-        return miles * 0.0035;
+        return cash;
     }
 
     public int getMilesValue() {
-        return (int) (this.cash / 0.0035);
+        return convertToMiles(this.cash);
     }
 }
