88b5214bcd543b194079cbd2f44aa957b6e76e38
PiyumiNadeera <dlpnadeera1234@gmail.com>
2023-12-02 20:09:12 +0530

    Adding RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+
+    public  RewardValue(double cashValue){
+            this.cashValue = cashValue;
+    }
+
+
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        double value = 0.0035 *cashValue;
+        return value;
+    }
+}
