9b1c69eedd7183758c1c0ddc3306052f6bbd9cd7
Jonathan <jonathanmoorevla@gmail.com>
2023-12-02 17:59:28 -0600

    Build out convert tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    private final double RATE = 0.0035f;
+
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -20,11 +22,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 200;
+        var testValue = new RewardValue(cashValue);
+        double expectValue = cashValue / RATE;
+        assertEquals(expectValue, testValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 12345;
+        var testValue = new RewardValue(milesValue);
+        double expectValue = milesValue * RATE;
+        assertEquals(expectValue, testValue.getCashValue());
     }
 }
