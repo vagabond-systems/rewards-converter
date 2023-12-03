2d0a2d2203a3b3eea671b91777f31dd9ca86583f
Shaun Maxwell <maxwell.shaun.0341@gmail.com>
2023-12-03 16:54:09 -0500

    Unit Tests completed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,27 +1,41 @@
 public class RewardValue {
 
     // CLASS VARIABLES
-    double cashValue;
-    int milesValue;
-    double ratio = 0.0035;
+    private final double cashValue;
+    public static final double ratio = 0.0035;
 
     // CONSTRUCTORS
     public RewardValue(double cashValue){
         this.cashValue=cashValue;
     };
     public RewardValue(int milesValue){
-        this.milesValue=milesValue;
+        this.cashValue = convertToCash(milesValue);
     };
 
 
     // METHODS
+    private static int convertToMiles(double cashValue){
+        return (int)(cashValue / ratio);
+    }
+
+    private static double convertToCash(int milesValue){
+        return milesValue * ratio;
+    }
+
+
     public double getCashValue(){
-        return ((double)milesValue / ratio);
+        return cashValue;
     };
 
 
     public double getMilesValue(){
-        return ((cashValue * ratio));
+        return convertToMiles(this.cashValue);
     };
+
+    // GETTERS AND SETTERS
+
+    public double getRatio() {
+        return this.ratio;
+    }
 
 }
