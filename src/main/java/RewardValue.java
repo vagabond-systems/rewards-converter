318c407b3afdd57b9f9494ff5f7bec9652283567
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-12-05 06:39:01 -0700

    add-attributes-and-constructor

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,15 @@
 public class RewardValue {
+    // Attributes
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor
+    public RewardValue(double value) {
+        this.cashValue = value;
+    }
+
+    //Methods
+    double getCashValue() {
+        return this.cashValue;
+    }
 }
