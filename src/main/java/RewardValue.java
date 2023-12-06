72f238387c5ffa92c0e1e57eafde0eb0d9f0eb85
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-12-06 14:13:51 -0600

    created RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,16 @@
+public class RewardValue {
+    private double cashValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+    public double getCashValue(){
+        return cashValue;
+    }
+    public double getMilesValue() {
+        // Perform calculations to convert cashValue to miles
+        // Example: Assuming a simple conversion rate of 1 dollar = 1 mile
+
+        return cashValue;
+    }
+}
