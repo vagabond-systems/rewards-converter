982d9ec96e588911662ff0b0faf473e97fa7c8db
unknown <garysmith193358@gmail.com>
2023-12-07 17:12:13 -0500

    added 2 constructors

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,14 @@
+public class RewardValue {
+    private final double cashValue;
+    private int miles;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+    }
+
+
+}
