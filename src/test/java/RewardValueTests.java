a4218ecf2d2b921a0827ed96e94734dc8d231359
Xiang <shy8870@outlook.com>
2023-12-08 19:36:19 +0000

    Add unit test for RewardConvert class

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(rewardValue.getMilesValue(), cashValue / RewardValue.CONVERT_RATE);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(rewardValue.getCashValue(), milesValue * RewardValue.CONVERT_RATE);
     }
 }
