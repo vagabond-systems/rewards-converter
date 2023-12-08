25964244503aded47ee5ba54afda024f4a0a2a70
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-12-08 09:34:04 -0600

    Added unit tests for RewardValue class

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -18,13 +18,33 @@ public class RewardValueTests {
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
+    /**
+     * Test case to ensure the correct conversion from cash to miles in the RewardValue class.
+     * It creates a RewardValue object with a specified cash value, calculates the expected miles
+     * value based on the conversion rate, and checks if the actual miles value matches the expected value.
+     *
+     * @see RewardValue
+     */
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 200;
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMilesValue = (int) (cashValue / RewardValue.VALUE_IN_MILES_RATE);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
+    /**
+     * Test case to ensure the correct conversion from miles to cash in the RewardValue class.
+     * It creates a RewardValue object with a specified miles value, calculates the expected cash
+     * value based on the conversion rate, and checks if the actual cash value matches the expected value.
+     *
+     * @see RewardValue
+     */
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 5000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue * RewardValue.VALUE_IN_MILES_RATE;
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
