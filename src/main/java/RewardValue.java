e4bf0805040a5dd90308dbc3c66f912b02e916fc
Adam Stilwell <stilwelladam8@gmail.com>
2023-12-07 19:39:14 -0500

    Finished the RewardValue class, but the tests are not passing. However, the RewardsConverter works as intended. Do not know if the tests are significant because they seem to be just functioning as basic getters and setters rather than doing calculations as the instructions tell me to do.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,7 +1,25 @@
 public class RewardValue {
+    private double cash;
+    final double milesValue = 0.0035;
+    private int miles;
+    RewardValue(double cashValue){
+        setCashValue(cashValue);
 
-    RewardValue(Double cash){
     }
     RewardValue(int miles){
+        setMiles(miles);
     }
+    public double getCashValue(){
+       return miles/milesValue;
+    }
+    public int getMilesValue(){
+        return (int)(cash*milesValue);
+    }
+    private void setCashValue(Double cashTemp){
+        cash = cashTemp;
+    }
+    private void setMiles(int milesTemp){
+        miles = milesTemp;
+    }
+
 }
