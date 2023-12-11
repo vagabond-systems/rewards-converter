7b53b53ca2aa795b0269ec6575eff647282eafb9
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-12-11 14:29:47 -0500

    Created RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,20 @@
+public class RewardValue {
+     double cashValue;
+     double milesValue;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+
+        this.milesValue = cashValue / 0.0035;
+    }
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+
+        this.cashValue = milesValue * 0.0035;
+    }
+    public double getCashValue() {
+        return cashValue;
+    }
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
