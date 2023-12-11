124279a3ec16d50c96f308f3e21c292d8742ed12
mannymontales <emmanmontales@gmail.com>
2023-12-11 13:25:55 -0500

    constructor and getters

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+
+    private final double cashValue;
+
+    //cash constructor
+    public RewardValue(double cashValue) {
+        this .cashValue = cashValue;
+    }
+
+    //miles constructor
+    public RewardValue (int milesValue) {
+        this.cashValue = milesValue * .0035;
+    }
+
+    //getters
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return (int) (this.cashValue / .0035);
+    }
+
+}
