f78476c2954de08f4523276e7efd4cd5325e47b1
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-12-11 15:13:57 -0500

    Changed the declaration

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,14 +1,12 @@
 public class RewardValue {
-     double cashValue;
-     double milesValue;
+    private final double cashValue;
+    private final double milesValue;
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-
         this.milesValue = cashValue / 0.0035;
     }
     public RewardValue(double milesValue, boolean isMiles) {
         this.milesValue = milesValue;
-
         this.cashValue = milesValue * 0.0035;
     }
     public double getCashValue() {
