a499693f4d1cca667424b4dd6356ba1895e615ed
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-11 19:43:40 -0500

    checkpointing my work added in values must be positive

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,12 +1,22 @@
 public class RewardValue {
     double cashValue = 100;
     int milesValue = 10000;
+    private static final double milesToCashConversionRate = 0.0035;
 
     // constructors
     public RewardValue(double cashValue) {
+        if(cashValue < 0) {
+            throw new IllegalArgumentException("Cash value must be positive");
+        }
+        this.cashValue = cashValue;
     }
 
     public RewardValue(int milesValue) {
+        if(milesValue < 0) {
+            throw new IllegalArgumentException("Miles value must be positive");
+        }
+        this.milesValue = milesValue;
+
     }
 
 
