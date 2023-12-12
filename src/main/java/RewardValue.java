70c80ba18f21fd856ce3ab46fb790adde1754c82
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-11 19:32:26 -0500

    checkpointing my work

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+public class RewardValue {
+    double cashValue = 100;
+    int milesValue = 10000;
+
+    // constructors
+    public RewardValue(double cashValue) {
+    }
+
+    public RewardValue(int milesValue) {
+    }
+
+
+    // methods
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
