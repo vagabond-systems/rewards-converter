2ef4cea764979bbc39d69460bd989e0777cdb83a
JLugo4 <jarek.lugo96@gmail.com>
2023-12-12 22:15:11 -0500

    RewardValue class created

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,28 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue;
+    }
+
+    public RewardValue(double milesValue, boolean isMiles){
+        if(isMiles){
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * 0.0035;
+        } else {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue / 0.0035;
+        }
+
+    }
+
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
