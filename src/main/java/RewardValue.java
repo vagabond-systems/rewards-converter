93185f07b472efc1f75d2c822f5b9f7f0b3b0b81
nathan <nathan.guardado@yahoo.com>
2023-12-12 22:17:09 -0500

    Created RewardValue class with conversions

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+
+    private double cashValue;
+    private int miles;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.miles = (int) (cashValue / 0.0035);
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+        this.cashValue = miles * 0.0035;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    public double getMilesValue() {
+        return this.miles;
+    }
+
+}
