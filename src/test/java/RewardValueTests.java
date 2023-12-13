73dd190c6c71412411f27d8087e691ebbc8a76b2
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-12-13 14:40:11 -0700

    add-displayName-testing-assert

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    @DisplayName("Create and verify cash value")
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -11,6 +13,7 @@ public class RewardValueTests {
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
+    @DisplayName("Create and verify miles values")
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
@@ -19,6 +22,7 @@ public class RewardValueTests {
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
+    @DisplayName("Convert from Cash to Miles values")
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
@@ -29,6 +33,7 @@ public class RewardValueTests {
 //        assert false;
     }
 
+    @DisplayName("Convert from Miles to Cash values")
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
