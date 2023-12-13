d765ee96d5403340a9dccdf5980943c4bf8ab07e
cgbridgewater <cgbridgewater@outlook.com>
2023-12-12 21:43:54 -0800

    debugged and fix issues as needed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+
+    double cashValue;
+    int milesValue;
+
+    // Takes cash and returns miles value at a rate of 285.71 (the reciprocal of 0.0035)
+    public int getMilesValue(int cashValue){
+
+        milesValue = (int) (cashValue * 285.71);
+
+        return milesValue;
+    }
+
+    // Takes miles and returns cash value at a rate of 0.0035
+    public double getCashValue(double milesValue){
+        cashValue = milesValue * 0.0035;
+
+        return cashValue;
+    }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -7,15 +7,15 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        var rewardValue = new RewardValue();
+        assertEquals(cashValue, rewardValue.getCashValue(cashValue));
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        var rewardValue = new RewardValue();
+        assertEquals(milesValue, rewardValue.getMilesValue(milesValue));
     }
 
     @Test
