8da2f9ac5a7e24868b0fd561a621b6729b2d2e37
cgbridgewater <cgbridgewater@outlook.com>
2023-12-13 00:32:47 -0800

    create test cases and test them

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -7,24 +7,30 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue();
-        assertEquals(cashValue, rewardValue.getCashValue(cashValue));
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue();
-        assertEquals(milesValue, rewardValue.getMilesValue(milesValue));
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 1000;
+        int expectedMiles = (int) ((int) 1000/0.0035);
+        var rewardsValue = new RewardValue(cashValue);
+        assertEquals(expectedMiles,rewardsValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double expectedCash = 10000 * 0.0035;
+        var rewardsValue = new RewardValue(milesValue);
+        assertEquals(expectedCash, rewardsValue.getCashValue());
     }
 }
