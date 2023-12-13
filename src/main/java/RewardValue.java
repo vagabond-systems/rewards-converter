ae8e0616d9a75746070450d60594439d70d87187
rlab874 <rlabreche419@gmail.com>
2023-12-13 02:57:36 -0500

    fixed conversion calculations

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,12 +1,23 @@
 public class RewardValue {
     private double cashValue;
-    private int milesvalue;
+    private int milesValue;
 
     public RewardValue(double cashValue){
+
         this.cashValue = cashValue;
     }
 
     public RewardValue(int milesvalue){
-        this.milesvalue = milesvalue;
+
+        this.milesValue = milesvalue;
+    }
+
+    public double getCashValue(){
+
+        return milesValue * 0.0035;
+    }
+
+    public int getMilesValue(){
+        return (int) (cashValue / 0.0035);
     }
 }
