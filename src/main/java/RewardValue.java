d2178be4e6eaeb46463ae092ca99a60d8f3bcefb
AnastasiaE3 <epiart2000@gmail.com>
2023-12-12 23:44:00 -0500

    added a second constructor for milesValue

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,11 +1,22 @@
 public class RewardValue {
 
     private double cashValue;
+    private double milesValue;
+
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
+    public RewardValue(double milesValue) {
+        this.milesValue = milesValue;
+    }
 
+    public double getCashValue() {
+        return cashValue;
+    }
 
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
