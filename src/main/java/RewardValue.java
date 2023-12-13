bdaa1df76b7e2de5b33d9e2339eeadd9d8222aeb
rlab874 <rlabreche419@gmail.com>
2023-12-13 02:31:54 -0500

    added rewardvalue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,12 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesvalue;
+
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesvalue){
+        this.milesvalue = milesvalue;
+    }
+}
