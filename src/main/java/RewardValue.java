699a5b06a6a5ae4ac49f17bd1f203fc6dabcd56b
AnastasiaE3 <epiart2000@gmail.com>
2023-12-13 00:16:48 -0500

    added getters and coversion methods

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -6,10 +6,12 @@ public class RewardValue {
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+        convertCashToMiles(cashValue);
     }
 
     public RewardValue(double milesValue) {
         this.milesValue = milesValue;
+        convertMilesToCash(milesValue);
     }
 
     public double getCashValue() {
@@ -18,5 +20,13 @@ public class RewardValue {
 
     public double getMilesValue() {
         return milesValue;
+    }
+
+    private void convertCashToMiles(double cashValue){
+        this.milesValue = cashValue * 0.0035;
+    }
+
+    private void convertMilesToCash(double milesValue){
+        this.cashValue = milesValue / 0.0035;
     }
 }
