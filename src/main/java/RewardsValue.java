f12c0c1f3495ff58c6a3880575f81c0eddb535fb
Gowri <gowri@Zipcoders-MacBook-Pro-64.local>
2023-12-14 16:42:31 -0500

    added constructors

---
+++ src/main/java/RewardsValue.java
@@ -0,0 +1,12 @@
+public class RewardsValue {
+    private double cashValue;
+    private int miles;
+
+    public RewardsValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardsValue(int miles) {
+        this.miles = miles;
+    }
+}
