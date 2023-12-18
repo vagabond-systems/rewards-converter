8e7ebea1807b402e70e76c9142c6e1f96887ab91
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-12-17 20:06:16 -0600

    Added test cases to test the conversion

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -24,7 +24,7 @@ public class RewardValueTests {
         int expectedMiles = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
         var rewardValue = new RewardValue(cashValue);
         assertEquals(expectedMiles, rewardValue.getMilesValue());
-    }
+    } 
 
     @Test
     void convert_from_miles_to_cash() {
