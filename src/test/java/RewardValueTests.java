a4325c219c4df4e6cbf33fcf4d1f67884a851561
MoniqueCMcQueen <monique.mcqueen@mykenzie.snhu.edu>
2023-12-18 14:17:32 -0500

    Updated RewardValue and Unit test

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,22 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
-    }
+        // cash to miles conversion
+        double cashValue = 350.00;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        // expected number of miles to convert
+        int milesIthinkIhave = (int) (cashValue * 0.0035);
 
+        assertEquals(milesIthinkIhave, rewardValue.getMilesValue());
+    }
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        // miles to cash
+        int milesValue = 5000;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        // expected cash to convert
+        double cashIHave = (milesValue / 0.0035);
+
+        assertEquals(cashIHave, rewardValue.getCashValue());
     }
 }
