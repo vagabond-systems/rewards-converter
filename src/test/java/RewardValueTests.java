1a65b952146daa84c7f516cba4b38c6cca1b5ee5
Sean <seanstevens729@gmail.com>
2023-12-19 09:58:57 -0500

    Added 2 tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import main.RewardValue;
 
 public class RewardValueTests {
 
@@ -18,13 +19,22 @@ public class RewardValueTests {
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
+    //Test decorator
+//void [name]
+//create an instance of the class and pass true
+//does the return match the expected ("delta" allows for small differences)
+
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 200; // Change cash value for testing
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue / 0.0035, rewardValue.getMilesValue(), 0.001); // Check conversion with a delta
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 5000; // Change miles value for testing
+        var rewardValue = new RewardValue(milesValue, true); // Pass 'true' to indicate miles value
+        assertEquals(milesValue * 0.0035, rewardValue.getCashValue(), 0.001); // Check conversion with a delta
     }
 }
