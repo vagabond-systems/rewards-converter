333af05d154a89bc1c9b3e90273b365f97a8b22d
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-12-20 14:28:02 -0500

    "Forage task1 complete"

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+   //cashValue constructor
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    //milesValue constructor
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+
+    //getter method returns cashValue
+    public double getCashValue() {
+        return cashValue;
+    }
+//getter method returns milesValue
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
