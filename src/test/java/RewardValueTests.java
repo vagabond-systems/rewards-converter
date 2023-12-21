c02b4fb728d978dc421fcb7a4c65e21de0413f3b
Lionel Mason <capitalcookhouse@gmail.com>
2023-12-20 21:15:12 -0500

    changes unit test back to original state for flow branch

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,20 +20,11 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-
-        int expectedMiles = (int) (cashValue / 0.0035);
-        assertEquals(expectedMiles, rewardValue.getMilesValue());
+        assert false;
     }
-
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-
-        double expectedCash = milesValue * 0.0035;
-        assertEquals(expectedCash, rewardValue.getCashValue());
+        assert false;
     }
 }
