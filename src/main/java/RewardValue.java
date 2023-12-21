5565758e0f4afd82201e5390d3746bf6bcbda5db
Munir Massoud <munirmassoud@yahoo.com>
2023-12-21 09:56:32 -0600

    completed

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,35 @@
+public class RewardValue {
+
+    private double cashValue;
+    private double milesValue;
+
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = convertingToMiles(cashValue);
+    }
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = convertingToCash(milesValue);
+    }
+
+
+    public double convertingToMiles(double cashValue){
+        return  cashValue / 0.0035;
+    }
+
+    public double convertingToCash(double milesValue){
+        return milesValue / 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+
+}
