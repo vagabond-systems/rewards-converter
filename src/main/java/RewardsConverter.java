46e199d7495b82b054e708005e309437caf3a0a1
Advait Shinde <105921012+adv-11@users.noreply.github.com>
2023-12-22 13:53:05 +0530

    Update RewardsConverter.java

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -18,3 +18,45 @@ public class RewardsConverter {
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
+
+//my code
+public class RewardValue {
+public double cashVal ;
+    public double milesVal;
+//constructors
+    public RewardValue(double cashVal)
+    {
+        this.cashVal=cashVal;
+        this.milesVal=convertCashToMiles(cashVal);
+    }
+    public RewardValue(double milesVal)
+    {
+        this.milesVal=milesValue;
+        this.cashVal=convertMilesToCash(milesVal);
+    }
+
+    //get the cash value
+    public double getCashValue() 
+    {
+        return cashVal;
+    }
+
+    //get the miles value
+    public double getMilesValue()
+    {
+        return milesVal;
+    }
+//cash to mile method
+     private double convertCashToMiles(double cashVal)
+    
+    {
+        return cashVal / 0.0035;
+    }
+
+    //mile to cash method
+    private double convertMilesToCash(double milesVal)
+    {
+        return milesVal * 0.0035;
+    }
+
+}
