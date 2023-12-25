5a9c0fe10c2e664bb006cf0ab66329b3063a37a6
shivaabhishek07 <shivaabhishek71@gmail.com>
2023-12-25 19:27:39 +0530

    Adds test suits

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -14,17 +14,21 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        var rewardValue = new RewardValue(milesValue,true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(28571.428, rewardValue.getMilesValue(), 0.001);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue,true);
+        assertEquals(35, rewardValue.getCashValue());
     }
 }
