b76d3291389410d471acc5a4c58e071a60307e53
shivaabhishek07 <shivaabhishek71@gmail.com>
2023-12-25 19:35:54 +0530

    Modifies test case

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
-
+    
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -22,13 +22,13 @@ public class RewardValueTests {
     void convert_from_cash_to_miles() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(28571.428, rewardValue.getMilesValue(), 0.001);
+        assertEquals(cashValue/0.0035, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue,true);
-        assertEquals(35, rewardValue.getCashValue());
+        assertEquals(milesValue*0.0035, rewardValue.getCashValue());
     }
 }
