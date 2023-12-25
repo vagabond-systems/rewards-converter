49c4aff4e9fd540882d9a45ffd0f887919f8b6a0
THARUNESHWAR-369 <s.tharuneshwar.369@gmail.com>
2023-12-25 11:19:54 +0530

    Tesst cases implemented

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -14,17 +14,25 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        var rewardValue = new RewardValue(milesValue, true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        // Create a RewardValue instance with a cash value
+        RewardValue reward = new RewardValue(100.0);
+
+        // Check if the conversion from cash to miles is correct
+        assertEquals(28571.428571428572, reward.getMilesValue(), 0.0001);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        // Create a RewardValue instance with a miles value
+        RewardValue reward = new RewardValue(5000.0, true);
+
+        // Check if the conversion from miles to cash is correct
+        assertEquals(17.5, reward.getCashValue(), 0.0001);
     }
 }
