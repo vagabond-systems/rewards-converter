ce34752f7bb0a5100bf1e5e7628ef657154a19b4
chrisratsabout <chrisratsabout@gmail.com>
2023-12-27 08:47:05 -0500

    fix unit tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,15 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 5;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(1428, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int miles = 1428;
+        var rewardValue = new RewardValue(miles);
+        assertEquals(4.998, rewardValue.getCashValue());
     }
 }
