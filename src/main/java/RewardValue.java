c57f5b38ccba31622699d45072db96b2597c8b7d
Venkata Sravan Telu <telusravan272@gmail.com>
2023-12-28 12:30:33 -0600

    Modifiec RewardValue.java for JUnit testing

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,14 +1,11 @@
 public class RewardValue{
-    double miles;
     double cashValue;
 
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.miles = cashValue * 0.0035;
     }
 
     public RewardValue(int miles){
-        this.miles = miles;
         this.cashValue = miles / 0.0035;
     }
 
@@ -18,6 +15,6 @@ public class RewardValue{
     }
 
     public double getMilesValue(){
-        return this.miles;
+        return this.cashValue*0.0035;
     }
 ;}
