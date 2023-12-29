c5a488ed26ff70deda4fb6ff2302e96418d5ead7
Anurag Kumar <anuragkr898@gmail.com>
2023-12-29 20:27:16 +0530

    corrections to RewardValue class + added new test cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,35 @@
 
 public class RewardValue {
 	
-	double cashValue;
-	int milesValue;
+	private double cashValue;
+	private int milesValue;
+	
+	public static final double CONVERSION_RATE = 0.0035;
 	
 	public RewardValue(double cashValue) {
 		this.cashValue = cashValue;
+		this.milesValue = convertToMiles(cashValue);
 	}
 	
 	public RewardValue(int milesValue) {
 		this.milesValue = milesValue;
+		this.cashValue = convertToCash(milesValue);
 	}
 	
-	public double getCashValue() {
-		double cash = milesValue * 0.0035; 
-		return cash;
+	private static int convertToMiles(double cashValue) {
+		return (int)(cashValue / CONVERSION_RATE);
+	}
+	
+	private static double convertToCash(int milesValue) {
+		return (milesValue * CONVERSION_RATE);
+	}
+	
+	public double getCashValue() { 
+		return cashValue;
 	}
 	
 	public int getMilesValue() {
-		int miles = (int) (cashValue / 0.0035);
-		return miles;
+		return milesValue;
 	}
 	
 }
