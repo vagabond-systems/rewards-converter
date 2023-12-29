585bbc39368a3a87df9e04a85ec098dc9f3cad60
Anthony <ovandoa26@gmail.com>
2023-12-28 22:25:36 -0500

    file name changed + fields added

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,11 @@
+public class RewardValue {
+    public class RewardValue {
+
+        // fields to store cash and miles values
+        private double cashValue;
+        private int milesValue;
+
+        // constant conversion rate from miles to cash
+        private static final double CONSTANT_RATE = 0.0035;
+
+}
