a3d548afebeb6f0a137c93e0d268db9f0284daa8
NagajothiRamesh <danamjothi1135626@gmail.com>
2023-12-31 00:08:13 +0530

    updated

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -21,10 +21,19 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         assert false;
+        double cashValue = 200;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue * 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
+
     }
 
     @Test
     void convert_from_miles_to_cash() {
         assert false;
+        int milesValue = 5000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue / 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue());
     }
 }
