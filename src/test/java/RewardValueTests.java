fdf1a1405eb996708e92a6e1eda66352dedb5836
Jim Grimes <jimgrimes86@gmail.com>
2023-12-30 10:03:07 -0500

    Add tests for convert_from_cash_to_miles and convert_from_miles_to_cash

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        // assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(Math.floor(cashValue/0.0035), rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        // assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue*0.0035, rewardValue.getCashValue());
     }
 }
