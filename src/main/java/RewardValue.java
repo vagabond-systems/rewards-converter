f9bfe8aa61275b6059d8ffad24fc627275a8a7c7
samokel <samowa456@hotmail.com>
2024-01-06 01:37:12 +0100

    modify RewardValue class.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,18 +2,16 @@ public class RewardValue {
     double cashValue;
     int milesValue;
     public RewardValue (double cash) {
-        cashValue = cash;
+        this.cashValue = cash;
     }
     public RewardValue (int miles) {
-        milesValue = miles;
+        this.milesValue = miles;
     }
     public double getCashValue() {
-        return this.milesValue * 0.0035;
+        return this.cashValue;
     }
 
     public int getMilesValue() {
-        double result;
-        result = RewardValue.this.cashValue / 0.0035;
-        return (int) result;
+        return (int) (this.cashValue / 0.0035);
     }
 }
