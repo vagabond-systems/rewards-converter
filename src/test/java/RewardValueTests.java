57d46d0ce463f6f45933fd26f209200a6651fdf3
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-01-07 01:35:07 -0500

    Test files updated

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 import RewardsConverter.java.RewardValue;
-=======
->>>>>>> f30cb5e67d23a2059d8e021ea1c6cfcb2e1e68be
+
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,12 +11,12 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-<<<<<<< HEAD
+
         RewardValue rewardValue;
         rewardValue = new RewardValue(cashValue);
-=======
+
         var rewardValue = new RewardValue(cashValue);
->>>>>>> f30cb5e67d23a2059d8e021ea1c6cfcb2e1e68be
+
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
@@ -29,27 +29,20 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-<<<<<<< HEAD
+
         double cash_to_miles_Value = 100;
         RewardValue rewardValue;
         rewardValue = new RewardValue(cash_to_miles_Value);
         assertEquals(cash_to_miles_Value, rewardValue.getCashValue());
-=======
-        assert false;
->>>>>>> f30cb5e67d23a2059d8e021ea1c6cfcb2e1e68be
+
     }
 
     @Test
     void convert_from_miles_to_cash() {
-<<<<<<< HEAD
+
         int miles_to_cash_Value = 10000;
         var rewardValue = new RewardValue(miles_to_cash_Value);
         assertEquals(miles_to_cash_Value, rewardValue.getMilesValue());
     }
 }
 //End of testing
-=======
-        assert false;
-    }
-}
->>>>>>> f30cb5e67d23a2059d8e021ea1c6cfcb2e1e68be
