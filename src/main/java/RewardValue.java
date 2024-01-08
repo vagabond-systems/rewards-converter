f74a1ab10d9bf46be966353623932913a074ddf3
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2024-01-08 23:46:30 +0530

    RewardValue done

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+
+    private static final double Miles_To_Cash = 0.0035;
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue (double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue/Miles_To_Cash);
+    }
+
+    public RewardValue (int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue*Miles_To_Cash;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
