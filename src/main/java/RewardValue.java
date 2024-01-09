127e7ca54c2bb358e6bfc715f6372c6804b528b3
Varun <VarunMK14@gmail.com>
2024-01-09 23:42:40 +0000

    Initial commit, created RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+public class RewardValue {
+
+    private double cashValue;
+    private double miles;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.miles = cashValue/0.0035;
+    }
+
+    public RewardValue(int miles) {
+        this.cashValue = miles*0.0035;
+        this.miles = miles;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    public double getMilesValue() {
+        return this.miles;
+    }
+}
