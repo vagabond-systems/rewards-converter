8320fef5b3f4a9629c3a3d229f15814bd812e799
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-01-11 14:52:23 -0600

    Refactor miles variable to milesValue

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,22 +1,22 @@
 public class RewardValue {
-    private int miles;
+    private int milesValue;
     private final double cashValue; //Add final keyword to ensure immutability
     public static final double MILES_TO_CASH_CONVERSION_RATE = .0035; //Use a clearer naming convention
 
     //Constructor - accepts cash value
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.miles = getMilesValue();
+        this.milesValue = getMilesValue();
     }
     //Constructor - accepts miles
-    public RewardValue(int miles){
-        this.miles = miles;
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
         this.cashValue = getCashValue();
     }
 
     // Returns the cash value of the RewardValue.
     public double getCashValue(){
-        return this.miles * MILES_TO_CASH_CONVERSION_RATE;
+        return this.milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     // Return how many miles the RewardValue is worth.
