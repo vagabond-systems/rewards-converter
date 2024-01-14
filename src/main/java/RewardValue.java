45717277f238fd06afc246bd29d1ec17dd794855
samokel <samowa456@hotmail.com>
2024-01-14 01:01:43 +0100

    modify RewardValueTest class with new test scenarios.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,29 @@
 public class RewardValue {
     double cashValue;
     int milesValue;
+    double miles_to_cash_conversion_unit = 0.0035;
+
     public RewardValue (double cash) {
         this.cashValue = cash;
     }
     public RewardValue (int miles) {
         this.milesValue = miles;
     }
+
+    public int cash_to_miles() {
+        double milesResult = cashValue / miles_to_cash_conversion_unit;
+        return (int) milesResult;
+    }
+
+    public double miles_to_cash() {
+        return milesValue * miles_to_cash_conversion_unit;
+    }
+
     public double getCashValue() {
         return this.cashValue;
     }
 
     public int getMilesValue() {
-        return (int) (this.cashValue / 0.0035);
+        return this.milesValue;
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,23 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double miles_to_cash_conversion_unit = 0.0035;
+        double cashValue = 100;
+        int milesValue = (int) (cashValue / miles_to_cash_conversion_unit);
+        var rewardValueCash = new RewardValue(cashValue);
+        var rewardValueMiles = new RewardValue(milesValue);
+        assertEquals(rewardValueMiles.milesValue, rewardValueCash.cash_to_miles());
+        //assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double miles_to_cash_conversion_unit = 0.0035;
+        int milesValue = 10000;
+        double cashValue = (double) milesValue * miles_to_cash_conversion_unit;
+        var rewardValueCash = new RewardValue(cashValue);
+        var rewardValueMiles = new RewardValue(milesValue);
+        assertEquals(rewardValueCash.cashValue, rewardValueMiles.miles_to_cash());
+        //assert false;
     }
 }
