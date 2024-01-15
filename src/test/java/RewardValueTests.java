a80fc58c1e4eaad2b4df73cfc3db28ca0e54bfc0
NhanNg52 <140850119+NhanNg52@users.noreply.github.com>
2024-01-15 14:56:55 -0800

    Add files via upload

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -13,18 +13,25 @@ public class RewardValueTests {
 
     @Test
     void create_with_miles_value() {
-        int milesValue = 10000;
+        int milesValue = 1000;
         var rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 200;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue/ 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.01);
+
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 1500;
+        var rewardValue = new RewardValue(milesValue, true);
+        double expectedMiles = milesValue * 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.01);
     }
 }
