db369c13179c29f809b849bf0c77116479c6fed1
Samuel <samuelou510@gmail.com>
2024-01-17 16:08:21 -0500

    conversion rate variable added for class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class RewardValue {
     private double cashValue;
+    private double conversionRate = 0.0035;
     private int milesValue;
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035);
+        this.milesValue = (int) (cashValue / this.conversionRate);
     }
     public RewardValue(int milesValue){
         this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035;
+        this.cashValue = milesValue * this.conversionRate;
     }
     public double getMilesValue(){
         return this.milesValue;
