adb8a5582bafca3c3e2f36684f44362d56450703
Johnny Wu <skydodle@gmail.com>
2024-01-19 16:38:58 +0800

    Added unit tests for cash-mile & mile-cash conversions

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,7 +1,7 @@
 public class RewardValue {
     private double cashValue;
     private double milesValue;
-    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     // Constructor for cash value
     public RewardValue(double cashValue) {
