1564f59bf64e7a772712ad6c81eada74354392ed
haile2020 <aryamhaile2019@gmail.com>
2024-01-25 22:31:26 -0500

    first commit Task 1

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,39 @@
 public class RewardValue {
-    private final double rate = 0.0035;
+    // Miles to cash conversion rate field
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    // cash field
     private double cash = 0.0;
+
+    // mile field
     private double miles =0.0;
 
+    // constructor
     public RewardValue(double cash){
-        this.miles = cash * rate;
-    }
-    public RewardValue(float miles) {
-        this.cash = miles/rate;
+        this.miles = this.CovertCashToMiles(cash);
     }
 
+    // constructor
+    public RewardValue(int miles) {
+        this.cash = this.CovertMilesToCash(miles);
+    }
+
+    // covert miles to cash method
+    public double CovertMilesToCash(int miles){
+        return  miles/MILES_TO_CASH_CONVERSION_RATE;
+    }
+    // covert cash to mile method
+    public double CovertCashToMiles(double cash){
+        return  cash * MILES_TO_CASH_CONVERSION_RATE;
+    }
 // 1 mile = $0.0035
 
+    // cash getter
     public double getCashValue() {
         return cash;
     }
 
+    // mile getter
     public double getMilesValue() {
         return miles;
     }
