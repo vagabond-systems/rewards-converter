3452c93378e22d9bf69379ea927d0cdee5afe7b6
Ratan <ratanrathod870@gmail.com>
2024-01-31 16:46:01 +0530

    added RewardValue java class in main package and tested working of code

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,36 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = convertToMiles(cashValue);
+    }
+
+    // Constructor that accepts miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    // Getter method for cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Getter method for miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    // Conversion method from cash to miles
+    private double convertToMiles(double cashValue) {
+        return cashValue / 0.0035;
+    }
+
+    // Conversion method from miles to cash
+    private double convertToCash(double milesValue) {
+        return milesValue * 0.0035;
+    }
+}
