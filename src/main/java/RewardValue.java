053273f69229624bf805b1d0328e7085f4eb830f
Marwa-934 <marwa.zerrouki@gmail.com>
2024-02-01 15:17:58 +0100

    Add RewardValue.java class
    Implement RewardValue class to calculate and manage reward points

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,28 @@
 public class RewardValue {
-    //Instance variables
-     private final double cashValue;
-     private final int milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    public  RewardValue(double cashValue){
-        this.cashValue=cashValue;
-        this.milesValue=(int)(cashValue/0.0035);
-    }
-    public RewardValue(int milesValue){
-        this.milesValue=milesValue;
-        this.cashValue=milesValue*0.0035;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    // getters
-     public double getCashValue(){
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
         return cashValue;
-     }
-    public int getMilesValue(){
-        return milesValue;
     }
 
-
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
