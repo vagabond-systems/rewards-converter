d930b277c41b8f4ecfc770d5448269065e135354
shruti <shrutivrathod@gmail.com>
2024-02-04 19:58:50 +0530

    Add RewardValue class with constructors and conversion methods

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -14,17 +14,31 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        RewardValue rewardValue = new RewardValue(milesValue, true);
+
         assertEquals(milesValue, rewardValue.getMilesValue());
+
+        // Check the calculated cash value based on the conversion rate
     }
 
-    @Test
-    void convert_from_cash_to_miles() {
-        assert false;
-    }
+
+
 
     @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+        void convert_from_cash_to_miles() {
+            double cashValue = 10000.0;
+            RewardValue reward = new RewardValue(cashValue);
+            double expectedMiles = 10000.0 / 0.0035;
+
+            assertEquals(expectedMiles, reward.getMilesValue(), 0.001);
+        }
+
+        @Test
+        void convert_from_miles_to_cash() {
+            double milesValue = 5000.0;
+            RewardValue reward = new RewardValue(milesValue, true);
+            double expectedCash = 5000.0 * 0.0035;
+
+            assertEquals(expectedCash, reward.getCashValue(), 0.001);
+        }
     }
-}
