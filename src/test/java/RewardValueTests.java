7368c003822aebbd766804ad7cdb00a29d4ec63a
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-17 11:02:00 +0200

    Task 1: Test objects - cash value & miles value - Automated commit at 2024-02-17 11:01:59

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+    // Test #1. Create rewardValue object with cash value
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -11,6 +12,7 @@ public class RewardValueTests {
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
+    // Test #2. Create rewardValue object with mile value
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
