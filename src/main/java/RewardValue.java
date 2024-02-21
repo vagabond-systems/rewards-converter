fbba6fa615685054b10ee454ad9e7b8d615aefee
Raj Vaishnav Tripathi <rajvaishnavtripathi@gmail.com>
2024-02-21 17:15:44 +0530

    Did some changes in RewardValue.java file for converion of cash_to_miles and completed remaining test cases in RewardValueTest file.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,32 @@
 public class RewardValue
 {
     double cashValue;
-    double milesValue;
+
     RewardValue(double cash)
     {
         cashValue = cash;
-    };
+    }
+
     RewardValue(int miles)
     {
-        milesValue = miles;
-    };
+        cashValue = milesToCash(miles);
+    }
+
+    int cashToMiles(double cashValue)
+    {
+        return (int) (cashValue / 0.0035);
+    }
+
+    double milesToCash(int milesValue)
+    {
+        return (milesValue * 0.0035);
+    }
+
     double getMilesValue()
     {
-        milesValue = cashValue/0.0035;
-        return milesValue;
+        return cashToMiles(cashValue);
     }
+
     double getCashValue()
     {
         return cashValue;
