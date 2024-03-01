2c58c59ec259c7ace7744420bd90c6c203de1b26
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-02 02:22:06 +0530

    RewardValue class added alongwith test cases

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private final double cashValue;
+    private final double milesValue;
+
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035; // Conversion from cash to miles
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035; // Conversion from miles to cash
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
