79127c466c662762711e06ad9b335009e006f4d3
kamachee <kamachee@wisc.edu>
2024-03-01 20:23:50 -0600

    RewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor accepting a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor accepting a value in miles
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Method to return the cash value of the RewardValue
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    // Method to return how many miles the RewardValue is worth
+    public double getMilesValue() {
+        return this.milesValue;
+    }
+}
