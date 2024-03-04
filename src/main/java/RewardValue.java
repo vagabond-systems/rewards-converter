8bfd128de7bb193f0f9e7fdf022b8c3c59ca48d9
Koushik chandra <koushikchandra@Koushiks-MBP.lan>
2024-03-04 12:05:14 -0500

    test

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,32 +1,30 @@
 public class RewardValue {
-
-    // Declare Variables
+    
+    /* Declare Variables */
     private final double cashValue;
-    private static final double CONVERSION_RATE = 0.0035; // Constants should be in uppercase
+    public static final double conversionRate = 0.0035;
 
-    // Constructors
+    /* Declare constructors */
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
-
-    public RewardValue(int milesValue) {
+    public RewardValue (int milesValue) {
         this.cashValue = convertToCash(milesValue);
     }
 
-    // Methods
-    public double getCashValue() {
-        return cashValue;
-    }
-
-    public int getMilesValue() {
-        return convertToMiles(this.cashValue);
-    }
-
+    /* Declare methods */
     private static int convertToMiles(double cashValue) {
-        return (int) (cashValue / CONVERSION_RATE);
+        return (int) (cashValue / conversionRate);
     }
 
     private static double convertToCash(int milesValue) {
-        return milesValue * CONVERSION_RATE;
+        return (double) (milesValue * conversionRate);
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -7,24 +6,34 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
+
+        RewardValue rewardValue = new RewardValue(cashValue);
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        int milesValue = 10_000;
+
+        RewardValue rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 10_000;
+        int expectedMilesValue = (int) (cashValue / RewardValue.conversionRate);
+
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10_000;
+        double expectedCashValue = milesValue * RewardValue.conversionRate;
+
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
