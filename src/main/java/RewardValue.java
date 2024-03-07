04609b986e9d8c15967fa2aa795148a12b5a48ef
Megha <meghashivaleela@gmail.com>
2024-03-07 11:40:46 +0530

    Task 1 updated

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,33 @@
 public class RewardValue {
 
-    private double cashValue;
-    private int miles;
+    private final double cashValue;
     private static final double RATE=0.0035;
 
     public RewardValue(double cashValue){
         this.cashValue=cashValue;
     }
+
     public RewardValue(int miles){
-        this.miles=miles;
+        this.cashValue=milestoCash(miles);
+    }
+
+    private static double milestoCash(int miles)
+    {
+        return miles*RATE;
+    }
+
+    private static int cashToMiles(double cashValue)
+    {
+        return (int) (cashValue/RATE);
     }
 
     public double getCashValue(){
-        cashValue=miles*RATE;
         return cashValue;
     }
 
     public int getMilesValue(){
-        miles=(int)Math.round(cashValue/RATE);
-        return miles;
+        return cashToMiles(this.cashValue);
     }
+
+
 }
