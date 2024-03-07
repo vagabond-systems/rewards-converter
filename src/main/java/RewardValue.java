04cc5155b51d5876eeb908abb603f61938bde4ff
Khusboo Patel <kkp5601@gmail.com>
2024-03-07 16:50:58 -0500

    Test

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,27 +1,27 @@
 public class RewardValue {
-        private double cashValue;
-        private double milesValue;
+    private final double cashValue;
 
-        // Constructor accepting cash value
-        public RewardValue(double cashValue){
-            this.cashValue=cashValue;
-            this.milesValue=cashValue/0.0035; // Conversion rate: 0.0035 miles per dollar
-        }
-
-        // Constructor accepting miles value
-        public RewardValue(double milesValue, boolean isMiles) {
-            this.milesValue = milesValue;
-            this.cashValue = milesValue * 0.0035; // Conversion rate: 0.0035 dollars per mile
-        }
-
-        // Getter method for cash value
-        public double getCashValue() {
-            return cashValue;
-        }
-
-        // Getter method for miles value
-        public double getMilesValue() {
-            return milesValue;
-        }
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
+}
