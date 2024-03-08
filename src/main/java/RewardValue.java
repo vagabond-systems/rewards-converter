67b973095ee06ed154b680cf98a7fd01f95a0415
rtova16 <rtova16@wgu.edu>
2024-03-08 11:35:21 -0700

    Improves RewardValue class by adding conversion methods and simplifies fields.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,29 @@
 public class RewardValue {
 
-    private double cashVal;
-    private int milesVal;
+    private final double cashValue;
+    private static final double CASH_TO_MILES_CONVERSION_RATE = 0.0035;
 
-    public RewardValue(double cashVal) {
-        this.cashVal = cashVal;
-        this.milesVal = (int) (cashVal / 0.0035);
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    public RewardValue(int milesVal) {
-        this.milesVal = milesVal;
-        this.cashVal = milesVal * 0.0035;
+    public RewardValue(int milesValue) {
+        cashValue = convertToCash(milesValue);
+    }
+
+    public static int convertToMiles(double cashValue) {
+        return (int) (cashValue / CASH_TO_MILES_CONVERSION_RATE);
+    }
+
+    public static double convertToCash(int milesValue) {
+        return milesValue * CASH_TO_MILES_CONVERSION_RATE;
     }
 
     public double getCashValue() {
-        return cashVal;
+        return cashValue;
     }
 
     public int getMilesValue() {
-        return milesVal;
+        return convertToMiles(cashValue);
     }
 }
