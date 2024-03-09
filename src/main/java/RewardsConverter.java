b30eaddf2248c92f9f67cc8b85ea2f8d77d3cb10
thesumedh <sumedhchandanshive2004@gmail.com>
2024-03-09 09:43:14 +0530

    Class added

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -7,6 +7,35 @@ public class RewardsConverter {
         System.out.println("Please enter a cash value to convert to airline miles: ");
         var input_value = scanner.nextLine();
         double cashValue;
+
+        // Define RewardValue class outside of main method
+        class RewardValue {
+            private double cashValue;
+            private int milesValue;
+        
+            // Constructor accepting cash value
+            public RewardValue(double cashValue) {
+                this.cashValue = cashValue;
+                this.milesValue = (int) (cashValue / 0.0035);
+            }
+        
+            // Constructor accepting miles value
+            public RewardValue(int milesValue) {
+                this.milesValue = milesValue;
+                this.cashValue = milesValue * 0.0035;
+            }
+        
+            // Method to get cash value
+            public double getCashValue() {
+                return cashValue;
+            }
+        
+            // Method to get miles value
+            public int getMilesValue() {
+                return milesValue;
+            }
+        }
+
         try {
             cashValue = Double.parseDouble(input_value);
         } catch (NumberFormatException exception) {
@@ -14,6 +43,7 @@ public class RewardsConverter {
             return;
         }
         System.out.println("converting $" + input_value + " to miles");
+        // Instantiate RewardValue class
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
