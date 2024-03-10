c99eb172d62ebcca3c65cfe5a19c2c66e10d2c51
PartySpirit <noelgladson7@gmail.com>
2024-03-10 12:27:33 +0530

    updated

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,18 +1,28 @@
 public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    double cashValue;
     public RewardValue(double cashValue) {
-
         this.cashValue = cashValue;
-
-
     }
-    public double getMilesValue(){
 
-        return this.cashValue * 0.0035;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
-    public double getCashValue(){
 
-        return this.cashValue ;
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
