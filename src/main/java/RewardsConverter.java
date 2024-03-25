219dfce00f9e9eb66514a269f30ad0d3f01ff25a
Hrushikesh Vootkuri <hrushikeshgoud1307@gmail.com>
2024-03-25 17:45:50 +0530

    new class has been added

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -17,4 +17,31 @@ public class RewardsConverter {
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
+    static class RewardValue {
+        private double cashValue;
+        private int milesValue;
+
+        // Constructor accepting cash value
+        public RewardValue(double cashValue) {
+            this.cashValue = cashValue;
+            this.milesValue = (int) (cashValue / 0.0035);
+        }
+
+        // Constructor accepting miles value
+        public RewardValue(int milesValue) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * 0.0035;
+        }
+
+        // Method to get cash value
+        public double getCashValue() {
+            return cashValue;
+        }
+
+        // Method to get miles value
+        public int getMilesValue() {
+            return milesValue;
+        }
+    }
+
 }
